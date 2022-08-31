8a945568fbfbcfd18203ec0a5f20903819269911
a93a <aviamirswe@gmail.com>
2022-09-01 00:07:52 +0200

    Create 322. Coin Change.kt

---
+++ leetcode in kotlin/322. Coin Change.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun coinChange(coins: IntArray, amount: Int): Int {
+        val dp = IntArray(amount+1){amount+1}
+        dp[0] = 0
+        for(a in 1..amount){
+            for(c in coins){
+                if(a-c >= 0)
+                    dp[a] = minOf(dp[a], 1 + dp[a-c])
+            }
+        }
+        if(dp[amount] != amount+1)
+            return dp[amount]
+        else
+            return -1
+    }
+}
