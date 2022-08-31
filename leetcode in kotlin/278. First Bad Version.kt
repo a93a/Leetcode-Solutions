928ed2498252451bf50742f7cedff6b1598b54bf
a93a <aviamirswe@gmail.com>
2022-09-01 00:10:30 +0200

    Create 278. First Bad Version.kt

---
+++ leetcode in kotlin/278. First Bad Version.kt
@@ -0,0 +1,18 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      fun isBadVersion(version: Int) : Boolean {} */
+
+class Solution: VersionControl() {
+    override fun firstBadVersion(n: Int) : Int {
+        var left = 1; var right = n
+        while(left < right){
+            val mid = left + (right - left)/2
+            val isBad = isBadVersion(mid)
+            if(isBad == true){
+                right = mid
+            }else{ //isBad == false
+                left = mid + 1
+            }
+        }
+        return left
+	}
+}
