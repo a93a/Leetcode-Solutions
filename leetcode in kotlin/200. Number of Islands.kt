ea892dff393cd33e06988c11dbe96cce53e68e93
a93a <aviamirswe@gmail.com>
2022-09-07 00:20:19 +0200

    Update 200. Number of Islands.kt

--- leetcode in kotlin/200. Number of Islands.kt
+++ leetcode in kotlin/200. Number of Islands.kt
@@ -72,6 +72,44 @@ class Solution {
         = (x >= 0 && y >= 0 && x < grid.size && y < grid[0].size && grid[x][y] == '1' && !visited[x][y])
 }
 
+//Using the Queue solution but implementing DFS by using the queue as a stack
+class Solution {    
+    fun numIslands(grid: Array<CharArray>): Int {
+        if(grid.size == 0)
+            return 0
+        val visited = Array(grid.size) { BooleanArray(grid[0].size) }
+        var noOfIslands = 0
+        for(r in 0..grid.size-1){
+            for(c in 0..grid[0].size-1){
+                if(grid[r][c]=='1' && !visited[r][c]){
+                    bfs(grid,r,c, visited)
+                    noOfIslands++
+                }
+            }
+        }      
+        return noOfIslands
+    }
+    private fun bfs(grid: Array<CharArray>, r: Int, c: Int, visited: Array<BooleanArray>) {
+        val q = ArrayDeque<Pair<Int,Int>>()
+        visited[r][c] == true
+        q.addLast(Pair(r,c))
+        while(!q.isEmpty()){
+            val (x,y) = q.removeLast()
+            val xCords = intArrayOf(0, 0, -1, 1)
+            val yCords = intArrayOf(-1, 1, 0, 0)
+            for(i in 0..3){
+                val newX = x+xCords[i]
+                val newY = y+yCords[i]
+                if(isValid(grid, newX, newY, visited)){
+                    q.addLast(Pair(newX,newY))
+                    visited[newX][newY] = true
+                }
+            }
+        }     
+    }
+    private fun isValid(grid: Array<CharArray>, x: Int, y: Int, visited: Array<BooleanArray>) 
+        = (x >= 0 && y >= 0 && x < grid.size && y < grid[0].size && grid[x][y] == '1' && !visited[x][y])
+}
 
 //using DFS with recursion
 class Solution {
