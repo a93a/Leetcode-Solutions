44a5a70cab96d68ccbdcedbed456d6db47b10b00
a93a <aviamirswe@gmail.com>
2022-10-06 13:35:28 +0200

    Create 232. Implement Queue using Stacks.kt

---
+++ leetcode in kotlin/232. Implement Queue using Stacks.kt
@@ -0,0 +1,41 @@
+class MyQueue() {
+    
+    val input = Stack<Int>()
+    val output = Stack<Int>()
+
+    fun push(x: Int) {
+        input.push(x)
+    }
+
+    fun pop(): Int {
+        if(output.isEmpty()){
+            repeat(input.size){
+                output.push(input.pop())
+            }
+        } 
+        return output.pop()
+    }
+
+    fun peek(): Int {
+        if(output.isEmpty()){
+            repeat(input.size){
+                output.push(input.pop())
+            }
+        } 
+        return output.peek()
+    }
+
+    fun empty(): Boolean {
+        return input.isEmpty() && output.isEmpty()
+    }
+
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
