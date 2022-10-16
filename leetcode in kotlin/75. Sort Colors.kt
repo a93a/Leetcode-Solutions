8001d1e3ad365a705c5b7eac11582507f484e83e
a93a <aviamirswe@gmail.com>
2022-10-16 16:14:29 +0200

    Update 75. Sort Colors.kt

--- leetcode in kotlin/75. Sort Colors.kt
+++ leetcode in kotlin/75. Sort Colors.kt
@@ -1,3 +1,33 @@
+// same algorithm as below but just a more kotlin way to write the condition loop
+class Solution {
+    fun sortColors(nums: IntArray): Unit {
+        var low = 0
+        var high = nums.size-1
+        var pointer = 0
+        while(pointer <= high){
+            when(nums[pointer]){
+                0 -> {
+                    swap(low,pointer,nums)
+                    low++
+                    pointer++
+                }
+                1 -> {
+                    pointer++
+                }
+                2 -> {
+                    swap(high,pointer,nums)
+                    high--
+                }
+            }
+        }
+    }
+    private fun swap(i: Int, j: Int, nums: IntArray){
+        val temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
+    }
+}
+
 class Solution {
     fun sortColors(nums: IntArray): Unit {
         var low = 0
