16d6a8eb991e4fabd24f8aa42c2aa04fcec377bf
a93a <aviamirswe@gmail.com>
2022-10-20 00:26:48 +0200

    Update 509. Fibonacci Number.kt

--- leetcode in kotlin/509. Fibonacci Number.kt
+++ leetcode in kotlin/509. Fibonacci Number.kt
@@ -1,3 +1,23 @@
+//seems to be the most optimal kotlin specific solution according to leetcode benchmark
+//even faster than solution 2 and uses around the same memory
+//however in my experience this benchmark is not so accurate, so i dont know for sure
+class Solution {
+    tailrec fun fib(n: Int): Int {
+        if(n <= 1) return n
+        return fib(n-1) + fib(n-2)
+    }
+}
+
+//addition of kotlins tailrec and a mutable map
+class Solution {
+    val cache = mutableMapOf(0 to 0, 1 to 1)
+    tailrec fun fib(n: Int): Int {
+        return cache.getOrPut(n) {
+            fib(n-1) + fib(n-2)
+        }
+    }
+}
+
 class Solution {
     fun fib(n: Int): Int {
         if(n <= 1) return n else{
@@ -14,11 +34,10 @@ class Solution {
     }
 }
 
-/**
+
 class Solution {
     fun fib(n: Int): Int {
         if(n <= 1) return n
         return fib(n-1) + fib(n-2)
     }
 }
-**/
