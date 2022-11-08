9a45c5cadbef37c071f1d5a0130de7dec88ee93e
a93a <aviamirswe@gmail.com>
2022-11-08 13:32:45 +0100

    79. Word Search memory optimisation, O(m*n) -> O(1)

--- leetcode in kotlin/79. Word Search.kt
+++ leetcode in kotlin/79. Word Search.kt
@@ -9,34 +9,34 @@ class Solution {
     
     fun exist(board: Array<CharArray>, word: String): Boolean {
         
-        fun isValid(i: Int, j: Int, visited: Array<BooleanArray>) = i in (0 until board.size) && j in (0 until board[0].size) && visited[i][j] == false
+        fun isValid(i: Int, j: Int) = i in (0 until board.size) && j in (0 until board[0].size) && board[i][j] != '.'
         
-        fun dfs(i: Int, j: Int, index: Int, visited: Array<BooleanArray>): Boolean {
+        fun dfs(i: Int, j: Int, index: Int): Boolean {
             if(board[i][j] != word[index] || index == word.length)
                 return false
             if(index == word.length-1) // and board[i][j] == word[index] is true here
                 return true
-            visited[i][j] = true
+            val temp = board[i][j]
+            board[i][j] = '.'
             var res = false
             for(d in dir){
                 val newI = i + d[0]
                 val newJ = j + d[1]
-                if(isValid(newI, newJ, visited)){
-                    res = res or dfs(newI, newJ, index+1, visited)
+                if(isValid(newI, newJ)){
+                    res = res or dfs(newI, newJ, index+1)
                 }
             }
             // backtracking, if all paths from here gave us false, we set this position as not visited...
             // ...as we might visit it again in another path. See example below
             if(res == false)
-                visited[i][j] = false
+                board[i][j] = temp
             return res
         }
         
         for(i in 0 until board.size) {
             for(j in 0 until board[0].size) {
                 if(word[0] == board[i][j]) {
-                    val visited = Array(board.size) {BooleanArray(board[0].size)}
-                    if(dfs(i,j,0, visited) == true)
+                    if(dfs(i,j,0) == true)
                         return true
                 }
             }
