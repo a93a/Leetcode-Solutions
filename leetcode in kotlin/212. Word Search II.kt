0fd61f9562e6cbda36ef638004553ba7aadc924d
a93a <aviamirswe@gmail.com>
2022-11-11 16:58:11 +0100

    Update 212. Word Search II.kt. more compact solution

--- leetcode in kotlin/212. Word Search II.kt
+++ leetcode in kotlin/212. Word Search II.kt
@@ -1,3 +1,79 @@
+// compact solution, same time complexity as below, but much faster and less memory according to my leetcode submissions
+class Solution {
+    
+    val dir = arrayOf(
+        intArrayOf(1,0),
+        intArrayOf(-1,0),
+        intArrayOf(0,1),
+        intArrayOf(0,-1)
+    )
+    
+    fun findWords(board: Array<CharArray>, words: Array<String>): List<String> {
+        
+        val res = ArrayList<String>()
+        val root: TrieNode? = TrieNode()
+        
+        fun isValid(i: Int, j: Int) = i in (0 until board.size) && j in (0 until board[0].size) && board[i][j] != '.'
+        
+        fun dfs(i: Int, j: Int, word: String, root: TrieNode?) {
+            
+            val c = board[i][j]
+            val nWord = word + c
+            val child = root?.child?.get(c - 'a')
+            
+            if(child == null) //prefix doesnt exist
+                return
+            
+            if(child.isEnd == true){ //word found 
+                res.add(nWord)
+                child.isEnd = false //"delete" the word from trie
+            }
+            
+            val temp = board[i][j]
+            board[i][j] = '.'
+             
+            for(d in dir){
+                val newI = i + d[0]
+                val newJ = j + d[1]
+                if(isValid(newI, newJ)){
+                    dfs(newI, newJ, nWord, child)
+                }
+            }
+            
+            board[i][j] = temp
+        }
+        
+        for(word in words){
+            var current = root
+            for(c in word){
+                if(current?.child?.get(c - 'a') == null)
+                    current?.child?.set(c - 'a', TrieNode())
+                current = current?.child?.get(c - 'a')
+            }
+            current?.isEnd = true
+        }
+        
+        for(i in 0 until board.size) {
+            for(j in 0 until board[0].size) {
+                val c = board[i][j]
+                if(root?.child?.get(c - 'a') != null)
+                    dfs(i,j, "", root)
+            }
+        }
+        
+        return res
+    }
+}
+
+class TrieNode() {
+    val child = arrayOfNulls<TrieNode>(26)
+    var isEnd = false
+}
+
+
+
+
+//non compact destructed solution with separate Trie logic (same time and space complexity)
 class Solution {
     
     val dir = arrayOf(
