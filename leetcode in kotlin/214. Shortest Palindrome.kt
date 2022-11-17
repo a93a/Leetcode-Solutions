589b745629fde15d80e99bd55f6c85bc7b09b1a6
a93a <aviamirswe@gmail.com>
2022-11-17 14:40:44 +0100

    Update 214. Shortest Palindrome.kt

--- leetcode in kotlin/214. Shortest Palindrome.kt
+++ leetcode in kotlin/214. Shortest Palindrome.kt
@@ -1,3 +1,24 @@
+//faster solution using KMP style LPS
+// huge difference in leetcode runtime and space 
+class Solution {
+    fun shortestPalindrome(s: String): String {
+        var rev = s.reversed()
+        var kmpS = s + "." + rev // "." as delimiter diving the string
+        var lps = IntArray(kmpS.length)
+        for(i in 1 until kmpS.length){
+            var j = lps[i-1]
+            while(j > 0 && kmpS[i] != kmpS[j])
+                j = lps[j - 1]
+            if(kmpS[i] == kmpS[j])
+                j++
+            lps[i] = j
+        }
+        return rev.substring(0, (s.length - lps[kmpS.length - 1])) + s
+    }
+}
+
+
+// pointer solutin
 class Solution {
     fun shortestPalindrome(s: String): String {
         var end = s.length-1
