97da308ef9a1c5c1ca185c2c92e16ffa375326f4
a93a <aviamirswe@gmail.com>
2023-01-19 11:21:42 +0100

    Update 1963-minimum-number-of-swaps-to-make-the-string-balanced.kt

--- leetcode in kotlin/1963-minimum-number-of-swaps-to-make-the-string-balanced.kt
+++ leetcode in kotlin/1963-minimum-number-of-swaps-to-make-the-string-balanced.kt
@@ -1,3 +1,34 @@
+// Same as below, using pointer, but more compact solution, time O(N), space O(1)
+class Solution {
+    fun minSwaps(s: String): Int {
+        var closed = 0
+        for(c in s){
+            if(c == '[') closed++
+            else if (closed > 0) closed--
+        }
+        return (closed + 1) / 2
+    }
+}
+
+// Solution using pointer (since we only have 2 chars to check), time O(N), space O(1)
+class Solution {
+    fun minSwaps(s: String): Int {
+        var mismatches = 0
+        var closed = 0
+        for(c in s){
+            if(c == ']') 
+                if(closed>0)
+                    closed--
+                else
+                    mismatches++
+            else{ 
+                closed++
+            }
+        }
+        return (mismatches + 1) / 2
+    }
+}
+
 // Stack solution, time O(N), space O(N)
 class Solution {
     fun minSwaps(s: String): Int {
