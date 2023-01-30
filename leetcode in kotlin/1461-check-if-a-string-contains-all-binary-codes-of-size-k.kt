b27875e3c953e1978672df7c75ee0ecb0ae3800d
a93a <aviamirswe@gmail.com>
2023-01-30 22:09:37 +0100

    Update 1461-check-if-a-string-contains-all-binary-codes-of-size-k.kt

--- leetcode in kotlin/1461-check-if-a-string-contains-all-binary-codes-of-size-k.kt
+++ leetcode in kotlin/1461-check-if-a-string-contains-all-binary-codes-of-size-k.kt
@@ -1,7 +1,10 @@
+/*
+* Sliding window technique
+*/
 class Solution {
     fun hasAllCodes(s: String, k: Int): Boolean {
         val hs = HashSet<String>()
-        val uniqueBits = Math.pow(2.0, k.toDouble()).toInt()
+        val uniqueBits = 1 shl k
         for(i in 0..s.lastIndex-k+1) {
             val str = s.substring(i, i + k)
             hs.add(str)
@@ -9,4 +12,11 @@ class Solution {
         }
         return false
     }
+}
+
+/*
+* Kotlin one line solution using Kotlin operations.
+*/
+class Solution {
+    fun hasAllCodes(s: String, k: Int) = s.windowed(k).toHashSet().size == 1 shl k
 }
