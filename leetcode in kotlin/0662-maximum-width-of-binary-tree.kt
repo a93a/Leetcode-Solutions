67922adbdbb5530f03709350fe3b43b151ab2504
Ali <aviamirswe@gmail.com>
2023-04-20 21:05:53 +0200

    Update 0662-maximum-width-of-binary-tree.kt

--- leetcode in kotlin/0662-maximum-width-of-binary-tree.kt
+++ leetcode in kotlin/0662-maximum-width-of-binary-tree.kt
@@ -8,17 +8,56 @@
  *     var right: TreeNode? = null
  * }
  */
+
+/*
+* BFS solution
+*/
 class Solution {
-    //time: O(n) and space(logN)
+    fun widthOfBinaryTree(root: TreeNode?): Int {
+        root?: return 0
+
+        val q = ArrayDeque<Triple<TreeNode?, Int, Int>>()
+        
+        var res = 0
+        var prevLevel = 0
+        var prevNum = 1
+        q.add(Triple(root, 1, 0))
+
+        while (q.isNotEmpty()) {
+            val (node, num, level) = q.poll()
+
+            if (level > prevLevel) {
+                prevLevel = level
+                prevNum = num
+            }
+
+            res = maxOf(res, num - prevNum + 1)
+
+            node?.left?.let {
+                q.add(Triple(node.left, 2 * num, level + 1)) 
+            }
+            node?.right?.let {
+                q.add(Triple(node.right, 2 * num + 1, level + 1)) 
+            }
+        }
+
+        return res
+    }   
+}
+
+/*
+* DFS solution
+*/
+class Solution {
     fun widthOfBinaryTree(root: TreeNode?): Int {
         var width = 0
         val levelMap = HashMap<Int, Int>()
         
         fun dfs(node: TreeNode?, depth: Int, pos: Int, levelMap: HashMap<Int, Int>) {
             node?: return
-            if(!levelMap.contains(depth)) //We do in order traversal of the tree, meaning we will reach the left most pos of each tree first
-                levelMap[depth] = pos // so we save this for each level of tree
-            width = maxOf(width, (pos - levelMap[depth]!! + 1)) // we will hit a max 
+            if(!levelMap.contains(depth))
+                levelMap[depth] = pos
+            width = maxOf(width, (pos - levelMap[depth]!! + 1))
             dfs(node.left, depth+1, 2*pos, levelMap)
             dfs(node.right, depth+1, 2*pos+1, levelMap)
         }
