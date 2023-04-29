7b444fb11739826b83a34e3b25feaefaadb7a6e0
Ali <aviamirswe@gmail.com>
2023-04-29 18:21:53 +0200

    Update 2657-find-the-prefix-common-array-of-two-array.kt

--- leetcode in kotlin/2657-find-the-prefix-common-array-of-two-array.kt
+++ leetcode in kotlin/2657-find-the-prefix-common-array-of-two-array.kt
@@ -1,3 +1,25 @@
+/*
+* A solution using Kotlins intersect(), same as below but much shorter
+*/
+class Solution {
+    fun findThePrefixCommonArray(A: IntArray, B: IntArray): IntArray {
+        val hsA = HashSet<Int>()
+        val hsB = HashSet<Int>()
+        var res = IntArray(A.size)
+        
+        for (i in A.indices) {    
+            hsA.add(A[i])
+            hsB.add(B[i])   
+            res[i] = (hsA intersect hsB).size
+        }
+        
+        return res
+    }
+}
+
+/*
+* The longer version
+*/
 class Solution {
     fun findThePrefixCommonArray(A: IntArray, B: IntArray): IntArray {
         val hsA = HashSet<Int>()
