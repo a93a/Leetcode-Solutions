ff6b5b0dd8ab320b6432d6eae79dda0011ca356d
Ali <aviamirswe@gmail.com>
2023-04-30 19:00:28 +0200

    Update 1319-number-of-operations-to-make-network-connected.kt

--- leetcode in kotlin/1319-number-of-operations-to-make-network-connected.kt
+++ leetcode in kotlin/1319-number-of-operations-to-make-network-connected.kt
@@ -1,4 +1,39 @@
 /*
+* Union-Find solution
+*/
+class Solution {
+    fun makeConnected(n: Int, connections: Array<IntArray>): Int {
+        if(connections.size < n - 1) return -1
+
+        val parent = IntArray(n) {it}
+
+        fun find(x: Int): Int {
+            if (parent[x] != x)
+                parent[x] = find(parent[x])
+            return parent[x]
+        }
+
+        fun union(x: Int, y: Int) {
+            val pX = find(x)
+            val pY = find(y)
+            if (pX != pY)
+                parent[pY] = pX
+        }
+
+        for ((x,y) in connections)
+            union(x, y)
+
+        var res = 0
+        for (i in 0 until n) {
+            if (find(i) == i)
+                res += 1
+        }
+        
+        return res - 1
+    }
+}
+
+/*
 * DFS solution
 */
 class Solution {
