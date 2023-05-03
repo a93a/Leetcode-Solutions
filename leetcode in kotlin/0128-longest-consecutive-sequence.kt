e14b13d4167697e966ebfadb8fd95440956bdfa4
Ali <aviamirswe@gmail.com>
2023-05-03 19:05:06 +0200

    Update 0128-longest-consecutive-sequence.kt

--- leetcode in kotlin/0128-longest-consecutive-sequence.kt
+++ leetcode in kotlin/0128-longest-consecutive-sequence.kt
@@ -1,4 +1,56 @@
 /*
+* Union Find solution
+*/
+lass Solution {
+
+    class DSU(val n: Int) {
+        val parent = IntArray(n) { it }
+        val size = IntArray(n) { 1 }
+
+        fun find(x: Int): Int {
+            if (parent[x] != x)
+                parent[x] = find(parent[x])
+            return parent[x]
+        }
+
+        fun union(x: Int, y: Int) {
+            val px = find(x)
+            val py = find(y)
+            if(px != py) {
+                parent[py] = px
+                size[px] += size[py]
+            }
+        }
+
+        fun getLongest(): Int {
+            var res = 0
+            for (i in parent.indices) {
+                if (parent[i] == i)
+                    res = maxOf(res, size[i])
+            }
+            return res
+        }
+    }
+    
+
+    fun longestConsecutive(nums: IntArray): Int {
+        val hm = HashMap<Int, Int>()
+        val dsu = DSU(nums.size)
+
+        for ((i,n) in nums.withIndex()) {
+            if (n in hm) continue
+
+            hm[n - 1]?.let { dsu.union(i, hm[n - 1]!!) }
+            hm[n + 1]?.let { dsu.union(i, hm[n + 1]!!) }
+
+            hm[n] = i
+        }
+
+        return dsu.getLongest()
+    }
+}
+
+/*
 * solution 1
 */
 class Solution {
