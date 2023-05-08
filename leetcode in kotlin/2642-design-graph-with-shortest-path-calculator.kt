e14aa679244d35e09682e4ab83086968fc098213
Ali <aviamirswe@gmail.com>
2023-05-08 21:11:57 +0200

    Create 2642-design-graph-with-shortest-path-calculator.kt

---
+++ leetcode in kotlin/2642-design-graph-with-shortest-path-calculator.kt
@@ -0,0 +1,50 @@
+class Graph(n: Int, edges: Array<IntArray>) {
+
+    val adj = HashMap<Int, ArrayList<Pair<Int, Int>>>().apply {
+        for (i in 0 until n)
+            this[i] = ArrayList<Pair<Int, Int>>()
+
+        for ((u, v, cost) in edges) {
+            this[u] = getOrDefault(u, ArrayList<Pair<Int, Int>>()).apply { this.add(v to cost) }
+        }
+    }
+
+    fun addEdge(edge: IntArray) {
+        val (u, v, cost) = edge
+        adj[u] = adj.getOrDefault(u, ArrayList<Pair<Int, Int>>()).apply { this.add(v to cost) }
+    }
+
+    fun shortestPath(node1: Int, node2: Int): Int {
+        val minHeap = PriorityQueue<Pair<Int, Int>> {a, b -> a.second - b.second }
+        val distance = IntArray(adj.size) { Integer.MAX_VALUE }
+
+        minHeap.add(node1 to 0)
+        distance[node1] = 0
+
+        while (minHeap.isNotEmpty()) {
+            val (node, cost) = minHeap.poll()
+
+            if (node == node2) return cost
+            if (cost > distance[node]) continue
+
+            adj[node]?.let {
+                for ((next, nextCost) in adj[node]!!) {
+                    if (cost + nextCost < distance[next]) {
+                        distance[next] = cost + nextCost
+                        minHeap.add(next to cost + nextCost)
+                    }
+                }
+            }
+        }
+
+        return -1
+    }
+
+}
+
+/**
+ * Your Graph object will be instantiated and called as such:
+ * var obj = Graph(n, edges)
+ * obj.addEdge(edge)
+ * var param_2 = obj.shortestPath(node1,node2)
+ */
