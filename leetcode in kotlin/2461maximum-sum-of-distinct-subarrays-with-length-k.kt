1bad79e7946961efb2e43d0feda4b62ca560d19d
Ali <aviamirswe@gmail.com>
2023-05-10 22:37:16 +0200

    Create 2461maximum-sum-of-distinct-subarrays-with-length-k.kt

---
+++ leetcode in kotlin/2461maximum-sum-of-distinct-subarrays-with-length-k.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun maximumSubarraySum(nums: IntArray, k: Int): Long {
+        var subArr = HashMap<Int, Int>()
+
+        var subArrSum = 0L
+        var res = 0L
+        for (i in 0..nums.lastIndex) {
+            subArr[nums[i]] = subArr.getOrDefault(nums[i], 0) + 1
+            subArrSum += nums[i]
+
+            if (i >= k - 1) {
+                if (subArr.size == k) res = maxOf(res, subArrSum)
+                subArrSum -= nums[i - k + 1]
+
+                val n = nums[i - k + 1]
+                subArr[n] = subArr[n]!! - 1
+                if (subArr[n] == 0) subArr.remove(n)
+            }
+        }
+
+        return res
+    }
+}
