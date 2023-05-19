87c95ed586bbca676822b1cfbc73ff8ca21e894b
Ali <aviamirswe@gmail.com>
2023-05-19 11:23:23 +0200

    Update 0785-is-graph-bipartite.kt

--- leetcode in kotlin/0785-is-graph-bipartite.kt
+++ leetcode in kotlin/0785-is-graph-bipartite.kt
@@ -35,3 +35,31 @@ class Solution {
         return true
     }
 }
+
+/*
+* DFS
+*/
+class Solution {
+    fun isBipartite(graph: Array<IntArray>): Boolean {
+        val color = IntArray(graph.size)
+
+        fun dfs(i: Int, c: Int): Boolean {
+            color[i] = c
+
+            for (n in graph[i]) {
+                if (color[n] == -1 * c) continue
+                else if (color[n] == color[i]) return false
+                else if (dfs(n, -1 * c) == false) return false
+            }
+            
+            return true
+        }
+        
+        for (i in 0 until graph.size) {
+            if (color[i] == 0 && dfs(i, 1) == false)
+                return false
+        }
+
+        return true
+    }
+}
