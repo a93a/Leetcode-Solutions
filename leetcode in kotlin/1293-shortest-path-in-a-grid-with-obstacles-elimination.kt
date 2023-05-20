461d578f2782961aa8a97be54ddfc9f6f7f883be
Ali <aviamirswe@gmail.com>
2023-05-20 16:13:49 +0200

    Update 1293-shortest-path-in-a-grid-with-obstacles-elimination.kt

--- leetcode in kotlin/1293-shortest-path-in-a-grid-with-obstacles-elimination.kt
+++ leetcode in kotlin/1293-shortest-path-in-a-grid-with-obstacles-elimination.kt
@@ -1,3 +1,51 @@
+/*
+* More optimized solution with O(m * n * k) time and O(m * n) space
+*/
+class Solution {
+    fun shortestPath(grid: Array<IntArray>, k: Int): Int {
+        val dir = arrayOf(
+            intArrayOf(1, 0),
+            intArrayOf(-1, 0),
+            intArrayOf(0, 1),
+            intArrayOf(0, -1)
+        )
+        val visited = Array (grid.size) { IntArray (grid[0].size) { -1 } }
+
+        fun isValid(i: Int, j: Int) = i in (0..grid.lastIndex) && j in (0..grid[0].lastIndex)
+    
+        with (LinkedList<IntArray>()) {
+            addLast(intArrayOf(0, 0, k, 0))
+            visited[0][0] = k
+
+            while (isNotEmpty()) {
+                val (i, j, obsLeft, len) = removeFirst()
+
+                if (i == grid.lastIndex && j == grid[0].lastIndex) 
+                    return len
+
+                for (d in dir) {
+                    val iNxt = i + d[0]
+                    val jNxt = j + d[1]
+                    if (isValid(iNxt, jNxt)) {
+                        val nxtObsLeft = if (grid[iNxt][jNxt] == 1) obsLeft - 1 else obsLeft
+                        if (nxtObsLeft > visited[iNxt][jNxt]) {
+                            if (nxtObsLeft != -1) {
+                                visited[iNxt][jNxt] = nxtObsLeft
+                                addLast(intArrayOf(iNxt, jNxt, nxtObsLeft, len + 1))
+                            }
+                        }  
+                    }
+                }
+            }
+        }
+
+        return -1
+    }
+}
+
+/*
+* More intuitive solution with O(m * n * k) time and O(m * n * k) space
+*/
 class Solution {
     fun shortestPath(grid: Array<IntArray>, k: Int): Int {
         val dir = arrayOf(
