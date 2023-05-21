6f3afb7447cc2560d86d1888e0f0e6f86d50420c
Ali <aviamirswe@gmail.com>
2023-05-21 19:00:10 +0200

    Update 0001-two-sum.kt

--- leetcode in kotlin/0001-two-sum.kt
+++ leetcode in kotlin/0001-two-sum.kt
@@ -1,16 +1,18 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {      
-        val result = IntArray(2)
-    val map: MutableMap<Int, Int> = HashMap()
-    for (i in nums.indices) {
-        if (map.containsKey(nums[i])) {
-            result[0] = i
-            result[1] = map[nums[i]]!!
-            break
-        } else {
-            map[target - nums[i]] = i
+        val res = intArrayOf(-1, -1)
+        val map = HashMap<Int, Int>()
+        
+        for (i in nums.indices) {
+            if (map.containsKey(nums[i])) {
+                res[0] = i
+                res[1] = map[nums[i]]!!
+                break
+            } else {
+                map[target - nums[i]] = i
+            }
         }
-    }
-    return result
+
+        return res
     }
 }
