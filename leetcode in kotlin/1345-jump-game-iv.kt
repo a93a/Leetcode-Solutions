8d2b17b3ec43ab5e0c3d1c5040e0b34584ef0712
Ali <aviamirswe@gmail.com>
2023-05-23 20:16:32 +0200

    Update 1345-jump-game-iv.kt

--- leetcode in kotlin/1345-jump-game-iv.kt
+++ leetcode in kotlin/1345-jump-game-iv.kt
@@ -1,41 +1,36 @@
 class Solution {
     fun minJumps(arr: IntArray): Int {
         val adj = HashMap<Int, ArrayList<Int>>()
-
-        for((i,v) in arr.withIndex()) {
-            adj[v] = adj.getOrDefault(v, arrayListOf()).apply{
-                this.add(i)
-            }
-        }
+        for((i,v) in arr.withIndex())
+            adj[v] = adj.getOrDefault(v, arrayListOf()).apply { add(i) }
        
-        val q = ArrayDeque<Int>()
-        q.add(0)
         var distance = 0
+        with (ArrayDeque<Int>()) {
+            add(0)
 
-        while(q.isNotEmpty()) {
-            repeat(q.size) {
+            while(isNotEmpty()) {
+                repeat(size) {
+                    val i = poll()
+                    
+                    if(i == arr.lastIndex)
+                        return distance
+                    if(i - 1 >= 0 && adj.contains(arr[i - 1]))
+                        add(i - 1)
+                    if(i + 1 < arr.size && adj.contains(arr[i + 1]))
+                        add(i + 1)
 
-                val i = q.poll()
-                if(i == arr.lastIndex)
-                    return distance
-
-                if(i - 1 >= 0 && adj.contains(arr[i - 1]))
-                    q.add(i - 1)
-
-                if(i + 1 < arr.size && adj.contains(arr[i + 1]))
-                    q.add(i + 1)
-
-                if(adj.contains(arr[i])) {
-                    for(j in adj[arr[i]]!!) {
-                        if(i != j) 
-                            q.add(j)
+                    adj[arr[i]]?.forEach {
+                        if(i != it) 
+                            add(it)
                     }
-                }
-                adj.remove(arr[i])
-            }
-            distance++ 
-        }
 
+                    adj.remove(arr[i])
+                }
+
+                distance++ 
+            }
+        }
+        
        return -1
     }
 }
