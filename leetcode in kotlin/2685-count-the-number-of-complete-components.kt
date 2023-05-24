416fb04a8e9762f16d82f61749f4b07d72eceb3e
Ali <aviamirswe@gmail.com>
2023-05-24 23:20:03 +0200

    Update 2685-count-the-number-of-complete-components.kt

--- leetcode in kotlin/2685-count-the-number-of-complete-components.kt
+++ leetcode in kotlin/2685-count-the-number-of-complete-components.kt
@@ -1,3 +1,50 @@
+/*
+* DFS
+*/
+class Solution {
+    fun countCompleteComponents(n: Int, edges: Array<IntArray>): Int {
+        val adj = HashMap<Int, ArrayList<Int>>().apply {
+            for ((u, v) in edges) {
+                this[u] = getOrDefault(u, arrayListOf()).apply { add(v) }
+                this[v] = getOrDefault(v, arrayListOf()).apply { add(u) }
+            }
+        }
+
+        val visited = BooleanArray(n)
+
+        fun bfs(i: Int): Pair<Int, Int> {
+            if (visited[i])
+                return 0 to 0
+
+            visited[i] = true
+
+            var nodes = 1
+            var edges = (adj[i]?.size?: 0)
+            adj[i]?.forEach {
+                val (nodes2, edges2) = bfs(it)
+                nodes += nodes2
+                edges += edges2
+            }
+
+            return nodes to edges
+        }
+
+        var res = 0
+        for (i in 0 until n) {
+            if (!visited[i]) {
+                val (nodes, edges) = bfs(i)
+                if ((nodes * (nodes - 1)) == edges)
+                    res++
+            }
+        }
+
+        return res
+    }
+}
+
+/*
+* Union Find
+*/
 class Solution {
     fun countCompleteComponents(n: Int, edges: Array<IntArray>): Int {
         val dsu = DSU(n)
