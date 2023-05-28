77cb1ce96c99d2ffd513dea774f0e518694efaea
Ali <aviamirswe@gmail.com>
2023-05-28 11:28:50 +0200

    Update 1547-minimum-cost-to-cut-a-stick.kt

--- leetcode in kotlin/1547-minimum-cost-to-cut-a-stick.kt
+++ leetcode in kotlin/1547-minimum-cost-to-cut-a-stick.kt
@@ -1,73 +1,29 @@
 class Solution {
-    
-    //time O(n^3) and space O((cuts.size+2)^2)
-    fun minCost(n: Int, cuts: IntArray): Int {
-        
-        val cList = cuts.toCollection(ArrayList())
-        cList.add(0)
-        cList.add(n)
-        cList.sort()
-        val dpMemo = Array(cList.size){IntArray(cList.size){-1}}
-        
-        fun dp(i: Int, j: Int): Int {
-            if(j - i <= 1) //size 1 or less
-                return 0
-            if(dpMemo[i][j] == -1){
-                dpMemo[i][j] = Integer.MAX_VALUE
-                for(c in i+1 until j){
-                    dpMemo[i][j] = minOf(dpMemo[i][j], dp(i, c) + dp(c, j) + (cList[j] - cList[i]))
-                }
-            }                   
-            return dpMemo[i][j]    
+    fun minCost(n: Int, c: IntArray): Int {
+        val cuts = c.toCollection(ArrayList()).apply {
+            add(0)
+            add(n)
+            sort()
         }
-    
-        return dp(0, cList.size-1)
-    }
-    
-}
 
-/**
-class Solution {
-    
-    //O(cuts.size * n^2) time complexity and O(n^2) space complexity
-    // fails the test because of memory usage, cant have O(n^2) space complexity
-    fun minCost(n: Int, cuts: IntArray): Int {
-        
-        // use of memoization to avoid repeated work
-        val dpMemo = Array(n+1){IntArray(n+1){-1}} // contains min cost of cutting from i to j
-     
-        fun dp(left: Int, right: Int): Int {
-            
-            if(dpMemo[left][right] != -1) // if already in memoization
-                return dpMemo[left][right]
-                 
-            var res = n * n + 1 //Integer.max_value bugs, i choose the largest integer that we cannopt reach,
-            // ,even if we have to cut at every index
-            var didCut = false // if we cant cut, return 0
-            
-            for(cut in cuts){
-                //if we can cut with this position
-                if(cut > left && cut < right){
-                    // if we can cut
-                    didCut = true
-                    // res will be min of:
-                    // current min or
-                    // min of mincost of (after cutting) left part of log, and right part of log and 
-                    // the cut cost of this cut (right - left)
-                    res = minOf(res, dp(left, cut) + dp(cut, right) + (right - left))
-                }
-            }
-            
-            if(didCut == false){
-                dpMemo[left][right] = 0
+        val dp = Array (cuts.size) { IntArray (cuts.size) { -1 } }
+
+        fun dfs(l: Int, r: Int): Int {
+            if (r - l <= 1)
                 return 0
-            }
-            
-            dpMemo[left][right] = res   
-            return res
+            if (dp[l][r] == -1) {
+                dp[l][r] = Integer.MAX_VALUE
+                for (c in l + 1 until r) {
+                    dp[l][r] = minOf(
+                        dp[l][r], 
+                        dfs(l, c) + dfs(c, r) + (cuts[r] - cuts[l])
+                    )
+                }
+            }    
+
+            return dp[l][r]    
         }
-        
-        return dp(0, n)
-    }
+    
+        return dfs(0, cuts.lastIndex)
+    } 
 }
-**/
