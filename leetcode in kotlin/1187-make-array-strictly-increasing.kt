029264cbc7d229a3bf5db0807fafb9a148e95130
Ali <aviamirswe@gmail.com>
2023-06-17 15:00:21 +0200

    Update 1187-make-array-strictly-increasing.kt

--- leetcode in kotlin/1187-make-array-strictly-increasing.kt
+++ leetcode in kotlin/1187-make-array-strictly-increasing.kt
@@ -1,13 +1,14 @@
 class Solution {
     fun makeArrayIncreasing(a1: IntArray, a2: IntArray): Int {
         val n = a1.size
+        val m = a2.size
         val dp = HashMap<Pair<Int, Int>, Int>()
 
         a2.sort()
 
         fun binarySearch(x: Int): Int {
             var left = 0
-            var right = a2.size
+            var right = m
             while (left < right) {
                 val mid = (left + right) / 2
                 if (a2[mid] <= x)
@@ -19,14 +20,14 @@ class Solution {
         }
 
         fun dfs(i: Int, prev: Int): Int {
-            if (i == a1.size) return 0
+            if (i == n) return 0
             dp[i to prev]?.let { return it }
 
             dp[i to prev] = 2001
             if (a1[i] > prev) dp[i to prev] = dfs(i + 1, a1[i])
 
             val index = binarySearch(prev)
-            if (index < a2.size) {
+            if (index < m) {
                 dp[i to prev] = minOf(
                     dp[i to prev] ?: 2001,
                     1 + dfs(i + 1, a2[index])
