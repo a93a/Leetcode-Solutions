5e5d69c5c3a544e0ce6fc86dec67b0a23f537065
Ali <aviamirswe@gmail.com>
2023-08-01 10:16:01 +0200

    Update 0077-combinations.kt

--- leetcode in kotlin/0077-combinations.kt
+++ leetcode in kotlin/0077-combinations.kt
@@ -1,15 +1,40 @@
+//without a loop
+class Solution {
+    fun combine(n: Int, k: Int): List<List<Int>> {
+        val res = ArrayList<ArrayList<Int>>()
+        val temp = ArrayList<Int>()
+
+        fun dfs(i: Int) {
+            if (temp.size == k) {
+                res.add(ArrayList(temp))
+                return
+            }
+            if (i > n) return
+
+            temp.add(0, i)
+            dfs(i + 1)
+            temp.removeAt(0)
+            dfs(i + 1)
+        }
+        
+        dfs(1)
+        return res
+    }
+}
+
+//with a loop
 class Solution {
     fun combine(n: Int, k: Int): List<List<Int>> {
 
         val res = ArrayList<ArrayList<Int>>()
 
         fun backtrack(start: Int, comb: ArrayList<Int>) {
-            if(comb.size == k) {
+            if (comb.size == k) {
                 res.add(ArrayList(comb))
                 return
             }
 
-            for(i in start..n) {
+            for (i in start..n) {
                 comb.add(i)
                 backtrack(i + 1, comb)
                 comb.removeAt(comb.size-1)
