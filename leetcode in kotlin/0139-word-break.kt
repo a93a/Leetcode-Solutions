dd8a6964d708621929ff37082f234a95d68c47ba
Ali <aviamirswe@gmail.com>
2023-08-04 12:46:34 +0200

    Update 0139-word-break.kt

--- leetcode in kotlin/0139-word-break.kt
+++ leetcode in kotlin/0139-word-break.kt
@@ -1,12 +1,15 @@
+//DP solution
 class Solution {
     fun wordBreak(s: String, wordDict: List<String>): Boolean {
-        val cache = BooleanArray(s.length+1)
-        cache[s.length] = true // lastPos is true, meaning [array of word length], lastPos] since string "" can be segmented of any list of any words (our dp basecase)
-        for(i in s.length-1 downTo 0){
-            for(word in wordDict){
-                if(word.length+i <= s.length){//enough chars in s for this word to be viable ?
-                    if(word == s.substring(i, i+word.length)){// its the same word char by char?
-                        if(cache[i+word.length] == true) //this cache index is true only if the cache at index [i+word.length is true]
+        val cache = BooleanArray (s.length + 1).apply {
+            this[s.length] = true 
+        }
+        
+        for (i in s.lastIndex downTo 0) {
+            for (word in wordDict) {
+                if (word.length + i <= s.length) {
+                    if (word == s.substring(i, i + word.length)) {
+                        if (cache[i + word.length] == true) 
                             cache[i] = true
                     }
                 }
