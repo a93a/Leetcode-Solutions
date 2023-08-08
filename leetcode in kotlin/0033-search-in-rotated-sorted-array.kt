e8f03d32e2ebf7b69def9f1d81d271d1a871a54b
Ali <aviamirswe@gmail.com>
2023-08-08 15:58:15 +0200

    Update 0033-search-in-rotated-sorted-array.kt

--- leetcode in kotlin/0033-search-in-rotated-sorted-array.kt
+++ leetcode in kotlin/0033-search-in-rotated-sorted-array.kt
@@ -1,23 +1,24 @@
 class Solution {
     fun search(nums: IntArray, target: Int): Int {
-        var l = 0; var r = nums.size-1     
-        while(l <= r){
+        var l = 0
+        var r = nums.size-1     
+        while (l <= r) {
             val m = (l + r)/2
-             if(target == nums[m])
+            if (target == nums[m]) 
                 return m
-            // right portion of sorted array
-            if(nums[l] <= nums[m]){
-                if(target > nums[m] || target < nums[l])
+            if (nums[l] <= nums[m]) {
+                if (target > nums[m] || target < nums[l])
                     l = m + 1
-                else // (target > nums[m] && target > nums[l])
+                else
                    r = m - 1
-            }else{ //left sorted portion of array
-                if(target < nums[m] || target > nums[r])
+            } else {
+                if (target < nums[m] || target > nums[r])
                     r = m - 1
-                else //(target < nums[m] && target > nums[r])
+                else
                     l = m + 1                  
             }
         }
+
         return -1
     }
 }
