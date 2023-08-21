b5e828317442feb690521f98c44d2748e892f67e
Ali <aviamirswe@gmail.com>
2023-08-21 15:27:56 +0200

    Update 0145-binary-tree-postorder-traversal.kt

--- leetcode in kotlin/0145-binary-tree-postorder-traversal.kt
+++ leetcode in kotlin/0145-binary-tree-postorder-traversal.kt
@@ -1,27 +1,17 @@
-/**
- * Example:
- * var ti = TreeNode(5)
- * var v = ti.`val`
- * Definition for a binary tree node.
- * class TreeNode(var `val`: Int) {
- *     var left: TreeNode? = null
- *     var right: TreeNode? = null
- * }
- */
-
 // iterative solution
 class Solution {
     fun postorderTraversal(root: TreeNode?): List<Int> {
         val res = ArrayList<Int>()
         val stack = Stack<TreeNode>()
                 
-        if(root != null) stack.push(root)
-        while(!stack.isEmpty()){
+        if (root != null) stack.push(root)
+        while (!stack.isEmpty()) {
             val node = stack.pop()
             res.add(0, node.`val`)
             if (node.left != null) stack.push(node.left)
             if (node.right != null) stack.push(node.right)
         }
+        
         return res
     }
 }
@@ -32,17 +22,16 @@ class Solution {
         val res = ArrayList<Int>()
         val stack = Stack<TreeNode>()
                 
-        if(root != null) stack.push(root)
-        while(!stack.isEmpty()){
+        if (root != null) stack.push(root)
+        while (!stack.isEmpty()) {
             
             val node = stack.pop()
             res.add(node.`val`)
             
             if(node.left != null) stack.push(node.left)
             if(node.right != null) stack.push(node.right)
-            
-            
         }
+        
         res.reverse()
         return res
     }
