ad631b5544176199cfbcaa13776b6518dbde3c4f
Ali <aviamirswe@gmail.com>
2023-09-03 20:23:38 +0200

    Update 0338-counting-bits.kt

--- leetcode in kotlin/0338-counting-bits.kt
+++ leetcode in kotlin/0338-counting-bits.kt
@@ -1,16 +1,34 @@
+// time O(n)
 class Solution {
     fun countBits(n: Int): IntArray {
-        val ans = IntArray(n+1)
-        ans[0] == 0
-        for(i in 1..n){
-            var _i = i
-            var count = 0
-            while(_i != 0){
-                if(_i and 1 == 1) count++
-                _i = _i shr 1
-            }
-            ans[i] = count
+        val res = IntArray (n + 1)
+
+        for (i in 1..n) {
+            res[i] = res[i / 2] + (i and 1)
         }
-        return ans
+
+        return res
+    }
+
+
+// time O(nlogn)
+class Solution {
+    fun countBits(n: Int): IntArray {
+        val res = IntArray (n + 1)
+
+        res[0] == 0
+        for (i in 1..n) {
+            var j = i
+            var count = 0
+
+            while (j != 0) {
+                if (j and 1 == 1) count++
+                j = j shr 1
+            }
+
+            res[i] = count
+        }
+
+        return res
     }
 }
