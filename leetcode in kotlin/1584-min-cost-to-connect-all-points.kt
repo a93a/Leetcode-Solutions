5de38aed7b04ac345c30327ba6585815ed301dcc
Ali <aviamirswe@gmail.com>
2023-09-15 14:00:13 +0200

    Update 1584-min-cost-to-connect-all-points.kt

--- leetcode in kotlin/1584-min-cost-to-connect-all-points.kt
+++ leetcode in kotlin/1584-min-cost-to-connect-all-points.kt
@@ -1,22 +1,23 @@
 class Solution {
     fun minCostConnectPoints(points: Array<IntArray>): Int {
-        val minHeap = PriorityQueue<Pair<Int,Int>> { a: Pair<Int, Int>, b: Pair<Int, Int> -> a.second - b.second } // sort by distance
-        val visited = HashSet<Int>()
-        var minCost = 0    
-        minHeap.add(Pair(0,0))
-        while(visited.size < points.size){
-            val (node,cost) = minHeap.poll()
-            if(visited.contains(node))
-                continue
-            minCost += cost
-            visited.add(node)
-            for(i in 0..points.size-1){
-                val (nextX,nextY) = points[i]
-                val (currentX, CurrentY) = points[node]
-                val distance = Math.abs(currentX - nextX) + Math.abs(CurrentY - nextY)
-                minHeap.add(Pair(i,distance))
+        val minHeap = PriorityQueue<IntArray> { a, b -> a[2] - b[2] }
+        val visited = HashSet<Pair<Int, Int>>()
+
+        var minCost = 0
+        minHeap.add(intArrayOf(points[0][0], points[0][1], 0))
+        while (visited.size < points.size) {
+            val (x, y, d) = minHeap.poll()
+            if ((x to y) in visited) continue
+
+            minCost += d
+            visited.add(x to y)
+
+            for ((x2, y2) in points) {
+                val d2 = Math.abs(x2 - x) + Math.abs(y2 - y)
+                minHeap.add(intArrayOf(x2, y2, d2))
             }
         }
+
         return minCost
     }
 }
