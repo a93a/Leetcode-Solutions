b2669a11d6be0cc7ee22447a5cb83ac65684b7ec
Ali <aviamirswe@gmail.com>
2023-10-05 13:23:25 +0200

    Update 0229-majority-element-ii.kt

--- leetcode in kotlin/0229-majority-element-ii.kt
+++ leetcode in kotlin/0229-majority-element-ii.kt
@@ -1,3 +1,4 @@
+// solution using hashmap, can easily convert into counting any "k-majority" of input array 
 class Solution {
     fun majorityElement(nums: IntArray): List<Int> {
         var count = HashMap<Int, Int>()
@@ -25,6 +26,46 @@ class Solution {
 
             if (numCount > (nums.size / 3)) res.add(n)
         }
+
+        return res
+    }
+}
+
+// specific for n/3 majority, without hashmap
+class Solution {
+    fun majorityElement(nums: IntArray): List<Int> {
+        var count1 = 0
+        var count2 = 0
+        var num1 = 0
+        var num2 = 0
+
+        for (num in nums) {
+            if (num == num1) {
+                count1++
+            } else if (num == num2) {
+                count2++
+            } else if (count1 == 0) {
+                count1 = 1
+                num1 = num
+            } else if (count2 == 0) {
+                count2 = 1
+                num2 = num
+            } else {
+                count1--
+                count2--
+            }
+        }       
+
+        val res = mutableListOf<Int>()
+        var numCount1 = 0
+        var numCount2 = 0
+        for (num in nums) {
+            if (num == num1) numCount1++
+            if (num == num2) numCount2++
+        }
+
+        if (numCount1 > nums.size / 3) res.add(num1)
+        if (numCount2 > nums.size / 3 && num1 != num2) res.add(num2)
 
         return res
     }
