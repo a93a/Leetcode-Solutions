90598c9177b1487b041b7a44bd6470c600ec34b3
Ali <aviamirswe@gmail.com>
2023-10-26 20:19:25 +0200

    Update 0767-reorganize-string.kt

--- leetcode in kotlin/0767-reorganize-string.kt
+++ leetcode in kotlin/0767-reorganize-string.kt
@@ -1,5 +1,43 @@
 class Solution {
     fun reorganizeString(s: String): String {
+        var count = HashMap<Char, Int>().apply {
+            for (c in s)
+                this[c] = getOrDefault(c, 0) + 1
+        }
+
+        val maxHeap = PriorityQueue<Pair<Int, Char>>() { a, b ->
+            b.first - a.first
+        }
+        
+        for ((ch, cnt) in count)
+            maxHeap.add(cnt to ch)
+
+        var prev: Pair<Int, Char>? = null
+        var res = StringBuilder()
+        while (maxHeap.isNotEmpty() || prev != null) {
+            if (maxHeap.isEmpty() && prev != null)
+                return ""
+
+            var (cnt, ch) = maxHeap.poll()
+            res.append(ch)
+            cnt--
+
+            prev?.let {
+                maxHeap.add(it)
+                prev = null
+            }
+
+            if (cnt != 0)
+                prev = cnt to ch
+        } 
+
+        return res.toString()
+    }
+}
+
+// another solution without heap
+class Solution {
+    fun reorganizeString(s: String): String {
         val freq = IntArray (26).apply {
             for (c in s) this[c - 'a']++
         }
