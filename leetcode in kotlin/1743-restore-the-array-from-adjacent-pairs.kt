46f92816e8723f964813aef484626f06514ecf03
Ali <aviamirswe@gmail.com>
2023-11-10 15:32:37 +0100

    Update 1743-restore-the-array-from-adjacent-pairs.kt

--- leetcode in kotlin/1743-restore-the-array-from-adjacent-pairs.kt
+++ leetcode in kotlin/1743-restore-the-array-from-adjacent-pairs.kt
@@ -33,3 +33,42 @@ class Solution {
         return res.toIntArray()
     }
 }
+
+// iterative dfs
+class Solution {
+    fun restoreArray(adjList: Array<IntArray>): IntArray {
+        val adj = HashMap<Int, MutableList<Int>>().apply {
+            for ((u, v) in adjList) {
+                getOrPut(u) { mutableListOf() }.apply { add(v) }
+                getOrPut(v) { mutableListOf() }.apply { add(u) }
+            }
+        }
+
+        var start = -1
+        for ((node, list) in adj) {
+            if (list.size == 1) {
+                start = node
+                break
+            }
+        }
+
+
+        val res = mutableListOf(start)
+        var cur = start
+        var prev = Integer.MAX_VALUE
+        while (res.size < adj.size) {
+            if (adj[cur] != null) {
+                for (nei in adj[cur]!!) {
+                    if (nei != prev) {
+                        res.add(nei)
+                        prev = cur
+                        cur = nei
+                        break
+                    }
+                }
+            }
+        }
+
+        return res.toIntArray()
+    }
+}
