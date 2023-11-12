e2b280adcaa7fd4956354dbfebf991f20593c463
Ali <aviamirswe@gmail.com>
2023-11-12 13:49:07 +0100

    Update 2642-design-graph-with-shortest-path-calculator.kt

--- leetcode in kotlin/2642-design-graph-with-shortest-path-calculator.kt
+++ leetcode in kotlin/2642-design-graph-with-shortest-path-calculator.kt
@@ -1,21 +1,19 @@
 class Graph(n: Int, edges: Array<IntArray>) {
-
     val adj = HashMap<Int, ArrayList<Pair<Int, Int>>>().apply {
         for (i in 0 until n)
-            this[i] = ArrayList<Pair<Int, Int>>()
-
+            getOrPut(i) { arrayListOf() }
         for ((u, v, cost) in edges) {
-            this[u] = getOrDefault(u, ArrayList<Pair<Int, Int>>()).apply { this.add(v to cost) }
+            getOrPut(u) { arrayListOf() }.apply { add(v to cost) }
         }
     }
 
     fun addEdge(edge: IntArray) {
         val (u, v, cost) = edge
-        adj[u] = adj.getOrDefault(u, ArrayList<Pair<Int, Int>>()).apply { this.add(v to cost) }
+        adj.getOrPut(u) { arrayListOf() }.apply { add(v to cost) }
     }
 
     fun shortestPath(node1: Int, node2: Int): Int {
-        val minHeap = PriorityQueue<Pair<Int, Int>> {a, b -> a.second - b.second }
+        val minHeap = PriorityQueue<Pair<Int, Int>> (compareBy { it.second })
         val distance = IntArray(adj.size) { Integer.MAX_VALUE }
 
         minHeap.add(node1 to 0)
@@ -27,24 +25,14 @@ class Graph(n: Int, edges: Array<IntArray>) {
             if (node == node2) return cost
             if (cost > distance[node]) continue
 
-            adj[node]?.let {
-                for ((next, nextCost) in adj[node]!!) {
-                    if (cost + nextCost < distance[next]) {
-                        distance[next] = cost + nextCost
-                        minHeap.add(next to cost + nextCost)
-                    }
+            adj[node]?.forEach { (next, nextCost) ->
+                if (cost + nextCost < distance[next]) {
+                    distance[next] = cost + nextCost
+                    minHeap.add(next to cost + nextCost)
                 }
             }
         }
 
         return -1
     }
-
 }
-
-/**
- * Your Graph object will be instantiated and called as such:
- * var obj = Graph(n, edges)
- * obj.addEdge(edge)
- * var param_2 = obj.shortestPath(node1,node2)
- */
