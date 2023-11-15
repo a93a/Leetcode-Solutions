294e16f24c9e0b58c345525bde85e33515de89ab
Ali <aviamirswe@gmail.com>
2023-11-15 21:53:16 +0100

    Update 0191-number-of-1-bits.kt

--- leetcode in kotlin/0191-number-of-1-bits.kt
+++ leetcode in kotlin/0191-number-of-1-bits.kt
@@ -1,29 +1,11 @@
-/*
-//For some reason, LeetCode gets an "time exceeded" error on all "unsigned Integers" where
-// the mostleft bit (bit #32)is 1, e.g 10000000......0
 class Solution {
-    // you need treat n as an unsigned value
     fun hammingWeight(n: Int): Int {
-        var sum = 0
-        var _n = n
-        while(_n != 0){
-            sum += _n and 1
-            _n = _n shr 1
+        var count = 0
+        var x = n
+        while (x != 0) {
+            count += (x and 1)
+            x = x ushr 1
         }
-        return sum
-    }
-}
-*/
-// Another solution
-class Solution {
-    // you need treat n as an unsigned value
-    fun hammingWeight(n: Int): Int {
-        var sum = 0
-        var binS = Integer.toBinaryString(n)
-        for(c in binS){
-            if(c=='1')
-                sum++
-        }
-        return sum
+        return count
     }
 }
