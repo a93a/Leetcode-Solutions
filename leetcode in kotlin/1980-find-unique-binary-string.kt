c242b3fc494677c0b415ddbd61cbcb0202b72411
Ali <aviamirswe@gmail.com>
2023-11-16 10:19:38 +0100

    Update 1980-find-unique-binary-string.kt

--- leetcode in kotlin/1980-find-unique-binary-string.kt
+++ leetcode in kotlin/1980-find-unique-binary-string.kt
@@ -2,19 +2,24 @@ class Solution {
     fun findDifferentBinaryString(nums: Array<String>): String {
         val hs = nums.toSet()
         val n = nums.size
-        var temp = CharArray(nums.size) { '0' }
+        var temp = CharArray (n) { '0' }
+        
+        fun s() = temp.joinToString("")
         
 
         fun backtrack(i: Int): Boolean {
-            if(i == n) {
-                return if(!hs.contains(temp.joinToString(""))) true else false
+            if (i == n) {
+                return if (!hs.contains(s())) true 
+                else false
             }
-            if(backtrack(i+1)) return true
+
+            if (backtrack(i + 1)) return true
             temp[i] = '1'
-            if(backtrack(i+1)) return true
+            if (backtrack(i + 1)) return true
+
             return false
         }
 
-        return if(backtrack(0)) temp.joinToString("") else ""
+        return if (backtrack(0)) s() else ""
     }
 }
