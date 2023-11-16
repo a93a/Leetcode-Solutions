555b6419433f623346eee2246c79217c63bb4203
Ali <aviamirswe@gmail.com>
2023-11-16 19:59:36 +0100

    Create 0762-prime-number-of-set-bits-in-binary-representation.kt

---
+++ leetcode in kotlin/0762-prime-number-of-set-bits-in-binary-representation.kt
@@ -0,0 +1,30 @@
+class Solution {
+    fun countPrimeSetBits(left: Int, right: Int): Int {
+        val prime = setOf(2, 3, 5, 7, 11, 13, 17, 19)
+
+        var res = 0
+        (left..right).forEach {
+            if (it.bits() in prime) res++
+        }
+
+        return res
+    }
+
+    fun Int.bits(): Int {
+        var x = this
+        var res = 0
+        while (x > 0) {
+            res += x and 1
+            x = x shr 1
+        }
+        return res
+    }
+}
+
+// or make use of kotlins functions
+class Solution {
+    fun countPrimeSetBits(left: Int, right: Int) = (left..right)
+        .map { it.countOneBits() }
+        .filter { it in setOf(2, 3, 5, 7, 11, 13, 17, 19) }
+        .count()
+}
