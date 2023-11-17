542f51b48a7aa37f72239e7f3e50c4758ac6bcac
Ali <aviamirswe@gmail.com>
2023-11-17 09:36:51 +0100

    Update 1877-minimize-maximum-pair-sum-in-array.kt

--- leetcode in kotlin/1877-minimize-maximum-pair-sum-in-array.kt
+++ leetcode in kotlin/1877-minimize-maximum-pair-sum-in-array.kt
@@ -9,3 +9,11 @@ class Solution {
         return res
     }
 }
+
+// or use Kotlin idomatic functions
+class Solution {
+    fun minPairSum(nums: IntArray) = nums.sorted()
+        .zip(nums.sortedDescending())
+        .map { it.first + it.second }
+        .max()
+}
