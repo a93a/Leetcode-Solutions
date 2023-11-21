f8f757ac304831a25c32b7a23085824518fc79a5
Ali <aviamirswe@gmail.com>
2023-11-21 13:02:57 +0100

    Create 1814-count-nice-pairs-in-an-array.kt

---
+++ leetcode in kotlin/1814-count-nice-pairs-in-an-array.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun countNicePairs(nums: IntArray): Int {
+        var mod = 1_000_000_000 + 7
+        val count = HashMap<Int, Int>()
+
+        var res = 0
+        nums.forEach { x ->
+            val y = x - x.rev()
+            res += count[y] ?: 0
+            res = res % mod
+            count[y] = (count[y] ?: 0) + 1
+        }
+
+        return res
+    }
+
+    fun Int.rev(): Int {
+        var x = this
+        var rev = 0
+        while (x != 0) {
+            rev *= 10
+            rev += x % 10
+            x /= 10
+        }
+        return rev
+    }
+}
