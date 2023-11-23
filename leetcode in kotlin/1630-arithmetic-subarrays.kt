ef4a5fb296f2bebee8a2e9ce6b9acf388ba91299
Ali <aviamirswe@gmail.com>
2023-11-23 12:17:16 +0100

    Update 1630-arithmetic-subarrays.kt

--- leetcode in kotlin/1630-arithmetic-subarrays.kt
+++ leetcode in kotlin/1630-arithmetic-subarrays.kt
@@ -1,3 +1,49 @@
+// O(m * n)
+class Solution {
+    fun checkArithmeticSubarrays(nums: IntArray, l: IntArray, r: IntArray): List<Boolean> {
+        val res = mutableListOf<Boolean>()
+
+        outer@ for ((i, j) in l.zip(r)) {
+            if (j - i < 2) {
+                res.add(true)
+                continue@outer
+            }
+
+            var min = Integer.MAX_VALUE
+            var max = Integer.MIN_VALUE
+            val set = HashSet<Int>()
+            for (k in i..j) {
+                min = minOf(min, nums[k])
+                max = maxOf(max, nums[k])
+                set.add(nums[k])
+            }
+
+            if (min == max) {
+                res.add(true)
+                continue@outer
+            }
+
+            val d = (max - min) / (j - i)
+            if ((max - min) % (j - i) != 0) {
+                res.add(false)
+                continue@outer
+            }
+
+            for (k in 1..(j - i)) {
+                if (!set.contains(min + k * d)) {
+                    res.add(false)
+                    continue@outer
+                }
+            }
+
+            res.add(true)
+        }
+
+        return res
+    }
+}
+
+// O(m * nlogn)
 class Solution {
     fun checkArithmeticSubarrays(nums: IntArray, l: IntArray, r: IntArray): List<Boolean> {
         val n = nums.size
@@ -16,7 +62,8 @@ class Solution {
     }
 }
 
-// less code with Kotlin functions
+// O(m * nlogn)
+// Same as above but with less code with Kotlin functions
 class Solution {
     fun checkArithmeticSubarrays(nums: IntArray, l: IntArray, r: IntArray) = l.zip(r)
         .map { (i, j) ->
