2ae224e6dc72700522e793d4befb684aea495eb5
Ali <aviamirswe@gmail.com>
2023-11-23 15:32:24 +0100

    Update 2928-distribute-candies-among-children-i.kt

--- leetcode in kotlin/2928-distribute-candies-among-children-i.kt
+++ leetcode in kotlin/2928-distribute-candies-among-children-i.kt
@@ -13,3 +13,17 @@ class Solution {
         return res
     }
 }
+
+// Use the solution from 2929. Distribute Candies Among Children II with the tougher constraints
+// O(n)
+class Solution {
+    fun distributeCandies(n: Int, limit: Int): Int {
+        var res = 0
+        for (firstTake in 0..minOf(n, limit)) {
+            val secondMax = minOf(limit, n - firstTake)
+            val secondMin = maxOf(0, n - firstTake - limit)
+            res += maxOf(secondMax - secondMin + 1, 0)
+        }
+        return res
+    }
+}
