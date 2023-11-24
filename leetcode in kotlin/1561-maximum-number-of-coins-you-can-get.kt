8408e42d6a579f739dcb68da19965dec32e10373
Ali <aviamirswe@gmail.com>
2023-11-24 11:14:32 +0100

    Update 1561-maximum-number-of-coins-you-can-get.kt

--- leetcode in kotlin/1561-maximum-number-of-coins-you-can-get.kt
+++ leetcode in kotlin/1561-maximum-number-of-coins-you-can-get.kt
@@ -1,3 +1,4 @@
+// Sort O(nlogn)
 class Solution {
     fun maxCoins(piles: IntArray): Int {
        piles.sort() 
@@ -16,4 +17,42 @@ class Solution {
         .dropLast(piles.size / 3)
         .chunked(2)
         .sumBy { it[1] }
+}
+
+// Counting sort O(n)
+class Solution {
+    fun maxCoins(piles: IntArray): Int {
+        val max = piles.max() ?: 10000
+        val count = IntArray (max + 1).apply {
+            piles.forEach { this[it]++ }
+        }
+
+        var res = 0
+        var toTake = piles.size / 3
+        var aliceTake = true
+        var num = max
+        while (num > 0) {
+            if (count[num] == 0) {
+                num--
+                continue
+            }
+            if (toTake == 0) break
+
+            if (!aliceTake) {
+                res += num
+                toTake--
+                aliceTake = true
+            } else {
+                aliceTake = false
+            }
+
+            if (count[num] > 1) {
+                count[num]--
+            } else {
+                num--
+            }
+        }
+
+        return res
+    }
 }
