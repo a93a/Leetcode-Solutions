a5ad02b1a590b58616f524cc1cb42764c52d7a36
Ali <aviamirswe@gmail.com>
2023-11-29 10:37:34 +0100

    Update 0088-merge-sorted-array.kt

--- leetcode in kotlin/0088-merge-sorted-array.kt
+++ leetcode in kotlin/0088-merge-sorted-array.kt
@@ -1,22 +1,17 @@
 class Solution {
-    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
-        var end = nums1.size-1; var _m = m-1; var _n = n-1
-        while(_m >= 0 && _n >= 0){
-            if(nums1[_m] > nums2[_n]){
-                nums1[end] = nums1[_m]
-                _m--
-            }else{ //nums1[mEnd] < nums2[nEnd]
-                nums1[end] = nums2[_n]
-                _n--
-            }
-            end--
+    fun merge(n1: IntArray, m: Int, n2: IntArray, n: Int): Unit {
+        var end = n1.lastIndex
+        var i = m - 1
+        var j = n - 1
+
+        while (i >= 0 && j >= 0) {
+            if (n1[i] > n2[j])
+                n1[end--] = n1[i--]
+            else
+                n1[end--] = n2[j--]
         }
-        //fill nums1 with leftover elements from nums2
-        while(_n >= 0){
-            System.out.println("$end $_n")
-            nums1[end] = nums2[_n]
-            _n--
-            end--
-        }
+
+        while (j >= 0)
+            n1[end--] = n2[j--]
     }
 }
