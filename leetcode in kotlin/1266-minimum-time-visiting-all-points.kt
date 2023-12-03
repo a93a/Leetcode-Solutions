1ea807c5b8188a750704601538c3d6bee89b5cd2
Ali <aviamirswe@gmail.com>
2023-12-03 10:49:54 +0100

    Update 1266-minimum-time-visiting-all-points.kt

--- leetcode in kotlin/1266-minimum-time-visiting-all-points.kt
+++ leetcode in kotlin/1266-minimum-time-visiting-all-points.kt
@@ -3,25 +3,59 @@ class Solution {
         var res = 0
         for (i in 1..points.lastIndex) {
             res += maxOf(
-                Math.abs(points[i][0] - points[i - 1][0]),
-                Math.abs(points[i][1] - points[i - 1][1])
+                abs(points[i][0] - points[i - 1][0]),
+                abs(points[i][1] - points[i - 1][1])
             )
         }
         return res
     }
 }
 
-// With fold()
+// bunch of different kotlin style solutions....
 class Solution {
     fun minTimeToVisitAllPoints(points: Array<IntArray>) = points
         .foldIndexed (0) { i, sum, point ->
             if (i > 0) {
                 sum + maxOf(
-                    Math.abs(points[i][0] - points[i - 1][0]),
-                    Math.abs(points[i][1] - points[i - 1][1])
+                    abs(points[i][0] - points[i - 1][0]),
+                    abs(points[i][1] - points[i - 1][1])
                 )
             } else {
                 sum
             }
+        }
+}
+
+class Solution {
+    fun minTimeToVisitAllPoints(points: Array<IntArray>) = points
+        .asSequence()
+        .zipWithNext()
+        .sumOf { (a, b) ->
+            maxOf(
+                abs(a[0] - b[0]), 
+                abs(a[1] - b[1])
+            )
+        }
+}
+
+class Solution {
+    fun minTimeToVisitAllPoints(points: Array<IntArray>) = points
+        .asSequence()
+        .windowed(2)
+        .sumOf { (a, b) ->
+            maxOf(
+                abs(a[0] - b[0]), 
+                abs(a[1] - b[1])
+            )
+        }
+}
+
+class Solution {
+    fun minTimeToVisitAllPoints(points: Array<IntArray>) = (1..points.lastIndex)
+        .sumOf { i ->
+            maxOf(
+                abs(points[i][0] - points[i - 1][0]), 
+                abs(points[i][1] - points[i - 1][1])
+            )
         }
 }
