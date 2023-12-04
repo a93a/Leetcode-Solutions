025111177e52914ecebaea1f4c85ffe32e3ccb9a
Ali <aviamirswe@gmail.com>
2023-12-04 15:28:38 +0100

    Update 0322-coin-change.kt

--- leetcode in kotlin/0322-coin-change.kt
+++ leetcode in kotlin/0322-coin-change.kt
@@ -1,16 +1,42 @@
+// DP
 class Solution {
     fun coinChange(coins: IntArray, amount: Int): Int {
-        val dp = IntArray(amount+1){amount+1}
+        val dp = IntArray (amount + 1) { amount + 1 }
         dp[0] = 0
-        for(a in 1..amount){
-            for(c in coins){
-                if(a-c >= 0)
-                    dp[a] = minOf(dp[a], 1 + dp[a-c])
+        for (a in 1..amount) {
+            for (c in coins) {
+                if (a-c >= 0)
+                    dp[a] = minOf(dp[a], 1 + dp[a - c])
             }
         }
-        if(dp[amount] != amount+1)
-            return dp[amount]
-        else
-            return -1
+
+        return if (dp[amount] != amount + 1) dp[amount] else -1
+    }
+}
+
+// Recursion + memoization
+class Solution {
+    fun coinChange(coins: IntArray, amount: Int): Int {
+        val dp = IntArray (amount + 1) { -1 }
+
+        fun dfs(amount: Int): Int {
+            if (amount == 0) return 0
+            if (dp[amount] != -1) return dp[amount]
+
+            var res = Integer.MAX_VALUE
+            for (coin in coins) {
+                if (amount - coin >= 0) {
+                    var count = dfs(amount - coin)
+                    if (count != Integer.MAX_VALUE)
+                        res = minOf(res, count + 1)
+                }
+            }
+
+            dp[amount] = res
+            return res
+        }
+
+        val res = dfs(amount)
+        return if (res == Integer.MAX_VALUE) -1 else res
     }
 }
