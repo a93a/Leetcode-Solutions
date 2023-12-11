76b23ba48fd3a77c13451e3d40461c86c7fc3028
Ali <aviamirswe@gmail.com>
2023-12-11 10:54:19 +0100

    Update 1223-dice-roll-simulation.kt

--- leetcode in kotlin/1223-dice-roll-simulation.kt
+++ leetcode in kotlin/1223-dice-roll-simulation.kt
@@ -30,3 +30,42 @@ class Solution {
         return res.toInt()
     }
 }
+
+// DP
+class Solution {
+    fun dieSimulator(n: Int, rollMax: IntArray): Int {
+        val mod = 1_000_000_007
+        val dp = Array (n) { Array (6) { LongArray (16) } }
+        
+        for (j in 0..5)
+            dp[0][j][1] = 1
+
+        for (i in 1 until n) {
+            for (j in 0..5) {
+                for (k in 1..rollMax[j]) {
+                    if (k > 1) {
+                        dp[i][j][k] = dp[i - 1][j][k - 1]
+                    } else {
+                        for (j2 in 0..5) {
+                            for (k2 in 1..rollMax[j2]) {
+                                if (j2 == j) continue
+                                dp[i][j][k] += dp[i - 1][j2][k2]
+                                dp[i][j][k] = dp[i][j][k] % mod
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        var res = 0L
+        for (j in 0..5) {
+            for (k in 1..rollMax[j]) {
+                res += dp[n - 1][j][k]
+                res %= mod
+            }
+        }
+
+        return res.toInt()
+    }
+}
