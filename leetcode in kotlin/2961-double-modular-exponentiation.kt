2603b07a93b016b70d0e059127139182d6eec50d
Ali <aviamirswe@gmail.com>
2023-12-13 20:24:12 +0100

    Update 2961-double-modular-exponentiation.kt

--- leetcode in kotlin/2961-double-modular-exponentiation.kt
+++ leetcode in kotlin/2961-double-modular-exponentiation.kt
@@ -9,12 +9,40 @@ class Solution {
         return res
     }
 
-    fun fastPow(a: Int, b: Int, m: Int): Int {
-        if (b == 0) return 1
-        if (b == 1) return a % m
-        val x = fastPow(a, b / 2, m)
-        if (b % 2 == 0)
-            return x * x % m
-        return (x * x * a) % m
+    // fast mod
+    fun fastPow(base: Int, exp: Int, mod: Int): Int {
+        if (mod == 1) return 0
+        var res = 1
+        for (e in 0 until exp) {
+            res = (res * base) % mod
+        }
+        return res
+    }
+}
+
+class Solution {
+    fun getGoodIndices(variables: Array<IntArray>, target: Int): List<Int> {
+        var res = mutableListOf<Int>()
+        for ((i, v) in variables.withIndex()) {
+            val (a, b, c, m) = v
+            if (fastPow(fastPow(a, b, 10), c, m) == target)
+                res.add(i)
+        }
+        return res
+    }
+
+    // fast mod binary method
+    fun fastPow(base: Int, exp: Int, mod: Int): Int {
+        if (mod == 1) return 0
+        var res = 1
+        var b = base % mod
+        var e = exp
+        while (e > 0) {
+            if (e % 2 == 1)
+                res = (res * b) % mod
+            e = e shr 1
+            b = (b * b) % mod
+        }
+        return res
     }
 }
