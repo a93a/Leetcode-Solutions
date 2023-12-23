fd5e740e2a9e583ad932b8ece692d2191e1bc140
Ali <aviamirswe@gmail.com>
2023-12-23 13:23:45 +0100

    Update 2477-minimum-fuel-cost-to-report-to-the-capital.kt

--- leetcode in kotlin/2477-minimum-fuel-cost-to-report-to-the-capital.kt
+++ leetcode in kotlin/2477-minimum-fuel-cost-to-report-to-the-capital.kt
@@ -1,24 +1,24 @@
 class Solution {
     fun minimumFuelCost(roads: Array<IntArray>, seats: Int): Long {
-
-        var result = 0.0
-        if (roads.size == 0) return result.toLong()
-
-        val adj = HashMap<Int, ArrayList<Int>>()
-        for (road in roads){
-            adj[road[0]] = adj.getOrDefault(road[0], ArrayList<Int>()).apply { this.add(road[1]) }
-            adj[road[1]] = adj.getOrDefault(road[1], ArrayList<Int>()).apply { this.add(road[0]) }
-        } 
+        var res = 0.0
+        if (roads.size == 0) 
+            return res.toLong()
+        
+        val adj = HashMap<Int, ArrayList<Int>>().apply {
+            for ((u, v) in roads) {
+                getOrPut(u) { arrayListOf() }.apply { add(v) }
+                getOrPut(v) { arrayListOf() }.apply { add(u) }
+            } 
+        }
         
         fun dfs(node: Int, parent: Int): Double {
-
             var passengers = 0.0
-            var res = 0.0
-            for (child in adj[node]!!) {
+
+            adj[node]?.forEach { child ->
                 if (child != parent) {
                     val childPassengers = dfs(child, node)
                     passengers += childPassengers
-                    result += Math.ceil(childPassengers / seats)
+                    res += Math.ceil(childPassengers / seats)
                 }
             }
 
@@ -26,6 +26,6 @@ class Solution {
         }
 
         dfs(0, -1)
-        return result.toLong()
+        return res.toLong()
     }
 }
