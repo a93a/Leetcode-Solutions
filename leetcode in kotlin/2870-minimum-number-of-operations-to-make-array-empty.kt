98704e5a2d0535eb81724717da8366b70a7c6f47
Ali <aviamirswe@gmail.com>
2024-01-04 22:27:08 +0100

    Update 2870-minimum-number-of-operations-to-make-array-empty.kt

--- leetcode in kotlin/2870-minimum-number-of-operations-to-make-array-empty.kt
+++ leetcode in kotlin/2870-minimum-number-of-operations-to-make-array-empty.kt
@@ -1,17 +1,11 @@
 class Solution {
     fun minOperations(nums: IntArray): Int {
-        val counts = HashMap<Int, Int>().apply {
-            nums.forEach {
-                this[it] = getOrDefault(it, 0) + 1
-            }
-        }
-
-        if ((counts.values.min() ?: 0) == 1) return -1
+        val count = nums.asIterable().groupingBy { it }.eachCount()
 
         var res = 0
-        counts.values.forEach {
-            res += (it / 3)
-            if ((it % 3) > 0) res ++
+        for (c in count.values) {
+            if (c == 1) return -1
+            res += (c / 3) + if (c % 3 > 0) 1 else 0
         }
 
         return res
