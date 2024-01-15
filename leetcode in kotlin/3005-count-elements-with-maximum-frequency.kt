52c828bc0f4550733d3f79e87157c16d8b95010b
Ali <aviamirswe@gmail.com>
2024-01-15 16:28:24 +0100

    Update 3005-count-elements-with-maximum-frequency.kt

--- leetcode in kotlin/3005-count-elements-with-maximum-frequency.kt
+++ leetcode in kotlin/3005-count-elements-with-maximum-frequency.kt
@@ -1,3 +1,4 @@
+// Same time complexity as below but with 2 passes a bit more "readability"
 class Solution {
     fun maxFrequencyElements(nums: IntArray): Int {
         val freq = IntArray (101)
@@ -10,6 +11,26 @@ class Solution {
         var res = 0
         for (n in freq)
             res += if (n == max) max else 0
+
+        return res
+    }
+
+// ...or in one pass!
+class Solution {
+    fun maxFrequencyElements(nums: IntArray): Int {
+        val freq = IntArray (101)
+        var max = -1
+        var res = 0
+        
+        for (n in nums) {
+            freq[n]++
+            if (freq[n] == max) {
+                res += max
+            } else if (freq[n] > max) {
+                max = freq[n]
+                res = max
+            }
+        }
 
         return res
     }
