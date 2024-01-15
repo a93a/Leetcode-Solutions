dd65aa83ea542578d5ba813ab10ca664e1d2cdfd
Ali <aviamirswe@gmail.com>
2024-01-15 12:36:17 +0100

    Update 2225-find-players-with-zero-or-one-losses.kt

--- leetcode in kotlin/2225-find-players-with-zero-or-one-losses.kt
+++ leetcode in kotlin/2225-find-players-with-zero-or-one-losses.kt
@@ -1,3 +1,4 @@
+// O(nlogn) time and O(n) space
 class Solution {
     fun findWinners(matches: Array<IntArray>): List<List<Int>> {
         val winners = HashMap<Int, Int>()
@@ -11,6 +12,24 @@ class Solution {
         return listOf(
             winners.filter { it.key !in losers }.map { it.key }.sorted(), 
             losers.filter { it.value == 1 }.map { it.key}.sorted()
+        )
+    }
+}
+
+// Optimized time O(n) and space O(n)
+class Solution {
+    fun findWinners(matches: Array<IntArray>): List<List<Int>> {
+        val winners = IntArray (100001)
+        val losers = IntArray (100001)
+
+        for ((w, l) in matches) {
+            winners[w]++
+            losers[l]++
+        }
+
+        return listOf(
+            winners.withIndex().filter { losers[it.index] == 0 && it.value != 0 }.map { it.index },
+            losers.withIndex().filter { it.value == 1 }.map { it.index }
         )
     }
 }
