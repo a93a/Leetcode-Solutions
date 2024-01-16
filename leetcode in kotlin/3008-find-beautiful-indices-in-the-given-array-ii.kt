7ce54f252b487a33a2947e3e2748060dae197bcb
Ali <aviamirswe@gmail.com>
2024-01-16 16:57:05 +0100

    Update 3008-find-beautiful-indices-in-the-given-array-ii.kt

--- leetcode in kotlin/3008-find-beautiful-indices-in-the-given-array-ii.kt
+++ leetcode in kotlin/3008-find-beautiful-indices-in-the-given-array-ii.kt
@@ -37,3 +37,50 @@ class Solution {
         return res
     }
 }
+
+// Rabin-Karp
+class Solution {
+    fun beautifulIndices(s: String, a: String, b: String, k: Int): List<Int> {
+        val AS = rabinKarp(s, a)
+        val BS = rabinKarp(s, b)
+
+        val res = mutableListOf<Int>()
+        var j = 0
+        for (i in AS.indices) {
+            while (j < BS.size && AS[i] > k + BS[j])
+                j++
+            if (j < BS.size && Math.abs(AS[i] - BS[j]) <= k)
+                res.add(AS[i])
+        }
+
+        return res
+    }
+
+    fun rabinKarp(a: String, s: String): List<Int> {
+        val d = 31
+        val q = 1_000_000_009L
+        val n = s.length
+        val m = a.length
+        val pPow = LongArray(maxOf(n, m)) { 1L }
+
+        for (i in 1 until pPow.size)
+            pPow[i] = (pPow[i - 1] * d) % q
+
+        val h = LongArray(m + 1) { 0L }
+        for (i in 0 until m)
+            h[i + 1] = (h[i] + (a[i] - 'a' + 1) * pPow[i]) % q
+
+        var hs = 0L
+        for (i in 0 until n)
+            hs = (hs + (s[i] - 'a' + 1) * pPow[i]) % q
+
+        val res = mutableListOf<Int>()
+        for (i in 0..(m - n)) {
+            val curH = (h[i + n] + q - h[i]) % q
+            if (curH == hs * pPow[i] % q)
+                res.add(i)
+        }
+
+        return res
+    }
+}
