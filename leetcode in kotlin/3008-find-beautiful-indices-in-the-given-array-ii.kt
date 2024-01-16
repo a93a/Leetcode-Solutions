981671d05289d9c3f4abc1a4bb389fbbe117826d
Ali <aviamirswe@gmail.com>
2024-01-16 15:42:40 +0100

    Create 3008-find-beautiful-indices-in-the-given-array-ii.kt

---
+++ leetcode in kotlin/3008-find-beautiful-indices-in-the-given-array-ii.kt
@@ -0,0 +1,39 @@
+// KMP
+class Solution {
+    fun beautifulIndices(s: String, a: String, b: String, k: Int): List<Int> {
+        val AS = kmp(s, a)
+        val BS = kmp(s, b)
+
+        val res = mutableListOf<Int>()
+        var j = 0
+        for (i in AS.indices) {
+            while (j < BS.size && AS[i] > k + BS[j])
+                j++
+            if (j < BS.size && Math.abs(AS[i] - BS[j]) <= k)
+                res.add(AS[i])
+        }
+
+        return res
+    }
+
+    private fun kmp(s: String, a: String): List<Int> {
+        val match = a + "#" + s
+
+        val lps = mutableListOf(0)
+        for (i in 1..match.lastIndex) {
+            var j = lps[i - 1]
+            while (j > 0 && match[j] != match[i])
+                j = lps[j - 1]
+            if (match[j] == match[i]) lps.add(j + 1)
+            else lps.add(0)
+        }
+
+        val res = mutableListOf<Int>()
+        for (i in 0..lps.lastIndex) {
+            if (lps[i] == a.length)
+                res.add(i - 2 * a.length)
+        }
+
+        return res
+    }
+}
