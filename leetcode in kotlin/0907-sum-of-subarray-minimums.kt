98c7280e2f6ae88e9804798e23f86512fac01af8
Ali <aviamirswe@gmail.com>
2024-01-17 20:41:47 +0100

    Update 0907-sum-of-subarray-minimums.kt
    +readability of the code

--- leetcode in kotlin/0907-sum-of-subarray-minimums.kt
+++ leetcode in kotlin/0907-sum-of-subarray-minimums.kt
@@ -1,14 +1,17 @@
 class Solution {
     fun sumSubarrayMins(arr: IntArray): Int {
-        val stack = LinkedList<Int>().apply { addLast(-1) }
-        val mod = 1_000_000_000 + 7
+        val stack = LinkedList<Int>()
+        val mod = 1_000_000_007
         var res = 0L
         
-        for (right in 0..arr.size) {            
-            while (stack.peekLast() != -1 && (if (right < arr.size) arr[right] else 0) < arr[stack.peekLast()]) {
+        for (right in 0..arr.size) {  
+            val curVal = if (right < arr.size) arr[right] else 0
+            
+            while (stack.isNotEmpty() && curVal < arr[stack.peekLast()]) {
                 val cur = stack.removeLast()
                 val left = stack.peekLast() ?: -1
-                res = (res + (cur.toLong() - left) * (right - cur) * arr[cur]) % mod
+                val noOfSubArrs = (cur.toLong() - left) * (right - cur)
+                res = (res + noOfSubArrs * arr[cur]) % mod
             }
             
             stack.addLast(right)
