0fa3fd94f0355c11adf2ab781a21ad2c027ef53c
Ali <aviamirswe@gmail.com>
2024-01-19 08:29:43 +0100

    Update 0931-minimum-falling-path-sum.kt

--- leetcode in kotlin/0931-minimum-falling-path-sum.kt
+++ leetcode in kotlin/0931-minimum-falling-path-sum.kt
@@ -1,16 +1,16 @@
 class Solution {
-    fun minFallingPathSum(matrix: Array<IntArray>): Int {
-
-        for (i in 1 until matrix.size) {
-            for (j in 0 until matrix.size) {
-                matrix[i][j] += minOf(
-                    if (j > 0) matrix[i - 1][j - 1] else 10001,
-                    matrix[i - 1][j],
-                    if (j < matrix.lastIndex) matrix[i - 1][j + 1] else 10001
+    fun minFallingPathSum(m: Array<IntArray>): Int {
+        val n = m.size
+        for (i in 1 until n) {
+            for (j in 0 until n) {
+                m[i][j] += minOf(
+                    if (j > 0) m[i - 1][j - 1] else 10001,
+                    m[i - 1][j],
+                    if (j + 1 < n) m[i - 1][j + 1] else 10001,
                 )
             }
         }
 
-        return matrix[matrix.lastIndex].min()!!
+        return m[n - 1].min()!!
     }
 }
