a4cb6e74abd69e97e0b8de898835b7bacdc24fba
Ali <aviamirswe@gmail.com>
2024-01-28 13:14:51 +0100

    Create 3020-find-the-maximum-number-of-elements-in-subset.kt

---
+++ leetcode in kotlin/3020-find-the-maximum-number-of-elements-in-subset.kt
@@ -0,0 +1,32 @@
+class Solution {
+    fun maximumLength(nums: IntArray): Int {
+        val nums = nums.map { it.toDouble() }
+        val count = nums
+            .toList()
+            .groupingBy { it }
+            .eachCount()
+            .toMutableMap()
+        
+        var res = 1
+        for (n in nums) {
+
+            if (n == 1.0) {
+                if ((count[n] ?: 0) % 2 == 0)
+                    count[n] = (count[n] ?: 0) - 1
+                res = maxOf(res, (count[n] ?: 0))
+                continue
+            }
+
+            var cur = 1
+            var next = sqrt(n)
+            while ((count[next] ?: 0) >= 2) {
+                cur += 2
+                next = sqrt(next)
+            }
+            
+            res = maxOf(res, cur)
+        }
+
+        return res
+    }
+}
