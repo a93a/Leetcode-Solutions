f5fb8743ceb280b0d9721085d91a194625ce3cf7
Ali <aviamirswe@gmail.com>
2024-02-10 13:26:12 +0100

    Update 0647-palindromic-substrings.kt

--- leetcode in kotlin/0647-palindromic-substrings.kt
+++ leetcode in kotlin/0647-palindromic-substrings.kt
@@ -1,69 +1,36 @@
-/*
-* Two pointer with outwards extension solution, time O(N^2) and space O(1)
-*/
+// Two pointer with outwards extension solution, time O(N^2) and space O(1)
 class Solution {
     fun countSubstrings(s: String): Int {
         var res = 0
 
-        for(i in s.indices) {
-
+        fun extend(i: Int) {
             var l = i
             var r = i
-
-            while(l >= 0 && r < s.length && s[l] == s[r]) {
-                res++
-                l--
-                r++
+            while (l >= 0 && r < s.length && s[l] == s[r]) {
+                l--; r++; res++
             }
-
             l = i
-            r = i+1
-
-            while(l >= 0 && r < s.length && s[l] == s[r]) {
-                res++
-                l--
-                r++
+            r = i + 1
+            while (l >= 0 && r < s.length && s[l] == s[r]) {
+                l--; r++; res++
             }
-
         }
+
+        s.indices.forEach { extend(it) }
         return res
     }
 }
 
-/*
-* Two pointer with outwards extension solution, but with helper function. time O(N^2) and space O(1)
-*/
+// DP solution, time O(N^2) and space O(N^2)
 class Solution {
     fun countSubstrings(s: String): Int {
+        val n = s.length
         var res = 0
-        fun extend(_l: Int, _r: Int) {
-            var l = _l
-            var r = _r
-            while(l >= 0 && r < s.length && s[l] == s[r]) {
-                res++
-                l--
-                r++
-            }
-        }
-        for(i in s.indices) {
-            extend(i,i)
-            extend(i,i+1)
-        }
-        return res
-    }
-}
-
-/*
-* DP solution, time O(N^2) and space O(N^2)
-*/
-class Solution {
-    fun countSubstrings(s: String): Int {
-        var res = 0
-        val dp = Array(s.length){ BooleanArray(s.length) }
-        for(i in s.lastIndex downTo 0) {
-            for(j in i..s.lastIndex) {
-                dp[i][j] = s[i] == s[j] && (j-i+1 < 3 || dp[i+1][j-1])
-                if(dp[i][j]) res++
+        val dp = Array (n) { BooleanArray(n) }
+        for (i in s.lastIndex downTo 0) {
+            for (j in i..s.lastIndex) {
+                dp[i][j] = s[i] == s[j] && (j - i + 1 < 3 || dp[i + 1][j - 1])
+                if (dp[i][j]) res++
             }
         }
         return res
