ff2e6dc91bdb0eb663fe5ecd997d9d7fdbd83e64
Ali <aviamirswe@gmail.com>
2024-02-20 09:59:24 +0100

    Update 0268-missing-number.kt

--- leetcode in kotlin/0268-missing-number.kt
+++ leetcode in kotlin/0268-missing-number.kt
@@ -1,14 +1,14 @@
 class Solution {
     fun missingNumber(nums: IntArray): Int {
         var amount = nums.size
-        for(i in nums.indices){
-            amount += i-nums[i]
+        for (i in nums.indices){
+            amount += i - nums[i]
         }
         return amount
     }
 }
 
-//another solution using xor
+// Another solution using xor
 class Solution {
     fun missingNumber(nums: IntArray): Int {
         var amount = nums.size
@@ -25,5 +25,14 @@ class Solution {
         var sum = (0..nums.size).reduce { a, v -> a + v }
         var sum2 = nums.sum() ?: 0
        return sum - sum2
+    }
+}
+
+// Sum by arithmetic sequence
+class Solution {
+    fun missingNumber(nums: IntArray): Int {
+        val n = nums.size
+        val sum = (n * (n + 1) / 2)
+        return sum - nums.sum()
     }
 }
