6782cad930d15d45dead540ab633959b4cff2f68
Ali <aviamirswe@gmail.com>
2024-02-28 11:11:08 +0100

    Update 0100-same-tree.kt

--- leetcode in kotlin/0100-same-tree.kt
+++ leetcode in kotlin/0100-same-tree.kt
@@ -1,21 +1,32 @@
-/**
- * Example:
- * var ti = TreeNode(5)
- * var v = ti.`val`
- * Definition for a binary tree node.
- * class TreeNode(var `val`: Int) {
- *     var left: TreeNode? = null
- *     var right: TreeNode? = null
- * }
- */
+// DFS recursive
 class Solution {
     fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
-        if(p == null && q == null)
-            return true
-        if(p == null || q == null)
-            return false
-        if(p.`val` != q.`val`)
-            return false
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+        if (p == null && q == null) return true
+        if (p == null || q == null) return false
+        if (p.`val` != q.`val`) return false
+        
+        return isSameTree(p.left, q.left) 
+            && isSameTree(p.right, q.right)
+    }
+}
+
+// BFS iterative
+class Solution {
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        val stack = LinkedList<Pair<TreeNode?, TreeNode?>> ()
+        stack.addLast(p to q)
+
+        while (stack.isNotEmpty()) {
+            val (p, q) = stack.removeLast()
+
+            if (p == null && q == null) continue
+            if (p == null || q == null) return false
+            if (p.`val` != q.`val`) return false
+
+            stack.addLast(p.left to q.left)
+            stack.addLast(p.right to q.right)
+        }
+
+        return true
     }
 }
