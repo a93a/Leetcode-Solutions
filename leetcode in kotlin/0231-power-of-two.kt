eaabd6d0639f9f60f53a77de41763aea689c8124
Ali <aviamirswe@gmail.com>
2024-03-02 15:28:56 +0100

    Update 0231-power-of-two.kt

--- leetcode in kotlin/0231-power-of-two.kt
+++ leetcode in kotlin/0231-power-of-two.kt
@@ -1,3 +1,17 @@
+// iterative
+class Solution {
+    fun isPowerOfTwo(n: Int): Boolean {
+        if (n == 0) return false
+        if (n == 1) return true
+
+        var n = n
+        while (n % 2 == 0)
+            n = n shr 1
+
+        return n == 1
+    }
+}
+
 // recursive
 class Solution {
     fun isPowerOfTwo(n: Int): Boolean {
@@ -7,20 +21,12 @@ class Solution {
     }
 }
 
-// iterative
+// one line bit manipulation
 class Solution {
-    public boolean isPowerOfTwo(int n) {
-        if (n == 0) return false;
-        if (n == 1) return true;
-        while (n % 2 == 0) {
-            n >>= 1;
-        }
-
-        return n == 1;
-    }
+    fun isPowerOfTwo(n: Int) = (n > 0) && (n and (n - 1) == 0)
 }
 
 // one line bit manipulation
 class Solution {
-    fun isPowerOfTwo(n: Int) = (n > 0) && (n and (n - 1) == 0)
+    fun isPowerOfTwo(n: Int) = (n > 0 && ((1 shl 30) % n) == 0)
 }
