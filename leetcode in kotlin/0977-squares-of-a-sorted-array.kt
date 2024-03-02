532b9d46036d0e4754a3ea04dc5b29b37541e9cd
Ali <aviamirswe@gmail.com>
2024-03-02 19:16:52 +0100

    Update 0977-squares-of-a-sorted-array.kt

--- leetcode in kotlin/0977-squares-of-a-sorted-array.kt
+++ leetcode in kotlin/0977-squares-of-a-sorted-array.kt
@@ -1,20 +1,23 @@
 class Solution {
     fun sortedSquares(nums: IntArray): IntArray {
-        var left = 0; var right = nums.size-1; var end = nums.size-1
-        val rArray = IntArray(nums.size)        
-        while(left <= right){ // or while(end >= 0)
+        var left = 0
+        var right = nums.lastIndex 
+        var end = nums.lastIndex
+        val res = IntArray (nums.size)    
+
+        while (left <= right) {
             val ls = nums[left] * nums[left]
             val rs = nums[right] * nums[right] 
-            if(ls > rs){
-                rArray[end] = ls
+            if (ls > rs) {
+                res[end] = ls
                 left++
-            }else{
-                //rs > ls
-                rArray[end] = rs
+            } else {
+                res[end] = rs
                 right--
             }
             end--
         }
-        return rArray
+
+        return res
     }
 }
