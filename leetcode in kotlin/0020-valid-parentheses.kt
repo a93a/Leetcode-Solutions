4004018c464ed170cdccde80cdcbaee28999fd16
Ali <aviamirswe@gmail.com>
2024-03-07 10:30:21 +0100

    Update 0020-valid-parentheses.kt

--- leetcode in kotlin/0020-valid-parentheses.kt
+++ leetcode in kotlin/0020-valid-parentheses.kt
@@ -19,3 +19,24 @@ class Solution {
         return leftSymbols.isEmpty()
     }
 }
+
+// Same logic as above, but add inverse symbols to stack.
+// We can do an early break when encoutering our first bad match.
+class Solution {
+    fun isValid(s: String): Boolean {
+        val stack = LinkedList<Char> ()
+
+        for (c in s) {
+            if (c == '(')
+                stack.addLast(')')
+            else if (c == '{')
+                stack.addLast('}')
+            else if (c == '[')
+                stack.addLast(']')
+            else if (stack.isEmpty() || stack.removeLast() != c)
+                return false
+        }
+
+        return stack.isEmpty()
+    }
+}
