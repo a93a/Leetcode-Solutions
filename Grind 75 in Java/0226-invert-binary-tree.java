dfc472dec2e172bca534970270c09fb68eda4bbb
Ali <aviamirswe@gmail.com>
2024-03-07 11:49:17 +0100

    Create 0226-invert-binary-tree.java

---
+++ Grind 75 in Java/0226-invert-binary-tree.java
@@ -0,0 +1,13 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        root.left = invertTree(right);
+        root.right = invertTree(left);
+
+        return root;
+    }
+}
