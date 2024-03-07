a01c3527f8d5659631843ad1ae930e887128bf87
Ali <aviamirswe@gmail.com>
2024-03-07 12:48:30 +0100

    Create 0278-first-bad-version.java

---
+++ Grind 75 in Java/0278-first-bad-version.java
@@ -0,0 +1,19 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        int res = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                res = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return res;
+    }
+}
