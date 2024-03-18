2cb931ae95b37e571e6196591b6ff8225900798f
Ali <aviamirswe@gmail.com>
2024-03-18 11:24:13 +0100

    Update 3084-count-substrings-starting-and-ending-with-given-character.kt

--- leetcode in kotlin/3084-count-substrings-starting-and-ending-with-given-character.kt
+++ leetcode in kotlin/3084-count-substrings-starting-and-ending-with-given-character.kt
@@ -1,16 +1,19 @@
 class Solution {
     fun countSubstrings(s: String, c: Char): Long {
         val n = s.length
-        val count = IntArray (n)
+        var count = 0
 
         for (i in (n - 1) downTo 0) {
-            if (s[i] == c) count[i] = 1
-            if (i < n - 1) count[i] += count[i + 1]
+            if (s[i] == c)
+                count++
         }
 
         var res = 0L
         for (i in s.indices) {
-            if (s[i] == c) res += count[i]
+            if (s[i] == c) {
+                res += count
+                count--
+            }
         }
 
         return res
