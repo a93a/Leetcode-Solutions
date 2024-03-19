c6edc026485c6415d5ee370fe37c4c2f5dbf2e92
Ali <aviamirswe@gmail.com>
2024-03-19 10:47:37 +0100

    Update 0621-task-scheduler.kt

--- leetcode in kotlin/0621-task-scheduler.kt
+++ leetcode in kotlin/0621-task-scheduler.kt
@@ -1,28 +1,35 @@
+// Use a priorityqueue
 class Solution {
     fun leastInterval(tasks: CharArray, n: Int): Int {
-        if(n == 0)
+        if (n == 0)
             return tasks.size
-        val hm = HashMap<Char,Int>()
-        for(task in tasks)
-            hm[task] = hm.getOrDefault(task, 0) + 1        
-        val maxHeap = PriorityQueue<Int>(compareBy{-it})
-        for(count in hm.values)
-             maxHeap.add(count) 
-        val q = ArrayDeque<Pair<Int,Int>>() // time, value
+
+        val hm = HashMap<Char,Int> ()
+        for (task in tasks)
+            hm[task] = hm.getOrDefault(task, 0) + 1     
+
+        val maxHeap = PriorityQueue<Int> (compareBy{ -it })
+        for (count in hm.values)
+            maxHeap.add(count) 
+
+        val q = ArrayDeque<Pair<Int,Int>> ()
         var time = 0
-        while(!maxHeap.isEmpty() || !q.isEmpty()){
-            if(!maxHeap.isEmpty()){
+        while (maxHeap.isNotEmpty() || q.isNotEmpty()) {
+            if (maxHeap.isNotEmpty()) {
                 var current = maxHeap.poll()
                 current--
-                if(current > 0)
-                    q.add(Pair(time+n,current))
+                if (current > 0)
+                    q.addLast((time + n) to current)
             }
-            if(!q.isEmpty()){
-                if(q.peek().first == time)
-                    maxHeap.add(q.poll().second)
+
+            if (q.isNotEmpty()) {
+                if (q.first().first == time)
+                    maxHeap.add(q.removeFirst().second)
             }
+
             time++
         }    
+
         return time          
     }
 }
