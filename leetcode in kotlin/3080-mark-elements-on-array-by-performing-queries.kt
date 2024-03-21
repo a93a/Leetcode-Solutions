eda55221e03b44d0b3217c126829d954af1a9e61
Ali <aviamirswe@gmail.com>
2024-03-21 19:03:49 +0100

    Create 3080-mark-elements-on-array-by-performing-queries.kt

---
+++ leetcode in kotlin/3080-mark-elements-on-array-by-performing-queries.kt
@@ -0,0 +1,34 @@
+class Solution {
+    fun unmarkedSumArray(n: IntArray, q: Array<IntArray>): LongArray {
+        val res = LongArray (q.size)
+        val min = PriorityQueue<IntArray> { a, b -> 
+            if (a[0] == b[0]) a[1] - b[1] else a[0] - b[0]
+        }
+
+        var sum = 0L
+        for ((i, x) in n.withIndex()) {
+            sum += x
+            min.add(intArrayOf(x, i))
+        }
+        val orgSum = sum
+
+        var r = 0
+        q.forEach { (i, k) ->
+            sum -= if (n[i] != -1) n[i] else 0
+            n[i] = -1
+
+            var p = 0
+            while (min.isNotEmpty() && p < k) {
+                val (_, j) = min.poll()
+                if (n[j] == -1) continue
+                sum -= if (n[j] != -1) n[j] else 0
+                n[j] = -1
+                p++
+            }
+
+            res[r++] = sum
+        }
+
+        return res
+    }
+}
