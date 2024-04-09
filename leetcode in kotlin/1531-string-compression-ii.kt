fb87fa69b6b8f2a7519c63a1342060b336fd8039
Ali <aviamirswe@gmail.com>
2024-04-09 10:54:18 +0200

    Update 1531-string-compression-ii.kt

--- leetcode in kotlin/1531-string-compression-ii.kt
+++ leetcode in kotlin/1531-string-compression-ii.kt
@@ -1,15 +1,16 @@
 class Solution {
     fun getLengthOfOptimalCompression(s: String, k: Int): Int {
         val cache = HashMap<String, Int>()
+        val set = setOf(1, 9, 99)
         
         fun count(i: Int, k: Int, prev: Char, prevCount: Int): Int {
             cache["$i:$k:$prev:$prevCount"]?.let { return it }
             if (k < 0) return Integer.MAX_VALUE
-            if (i == s.length) return 0
+            if (s.length - i == k) return 0
 
             var res = -1
             if (s[i] == prev) {
-                val incr = if (prevCount in setOf(1, 9, 99)) 1 else 0
+                val incr = if (prevCount in set) 1 else 0
                 res = incr + count(i + 1, k, s[i], prevCount + 1)
             } else {
                 res = minOf(
